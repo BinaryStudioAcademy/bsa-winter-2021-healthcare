b7c4dd2f44d18c221bc0f7a1b93b9f141076fe2a
what1s1ove <zubko.2007@gmail.com>
2021-02-27 15:52:41 +0200

    development: + frontend index file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import * as React from 'react';
+import { render } from 'react-dom';
 import App from './app';
+import './assets/styles/index.scss';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
