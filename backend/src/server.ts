5779ee1d488e60494f0b60a2fa1566dffc0423ab
what1s1ove <zubko.2007@gmail.com>
2021-02-28 10:55:06 +0200

    development: + base backend structure

---
+++ backend/src/server.ts
@@ -0,0 +1,22 @@
+import { join } from 'path';
+import express, { json, urlencoded } from 'express';
+import { ENV } from '~/common/enums';
+import config from '../package.json';
+
+const app = express();
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+app.use(express.static(join(__dirname, '../public')));
+app.use('/*', (_req, res) => {
+  return res.sendFile(join(__dirname, '../public', 'index.html'));
+});
+
+const server = app.listen(ENV.APP.SERVER_PORT, () => {
+  console.info(
+    `Listening to connections on port — ${ENV.APP.SERVER_PORT} on node — v${config.engines.node}`,
+  );
+});
+
+export { server };
