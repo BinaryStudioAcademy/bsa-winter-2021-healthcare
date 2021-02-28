ddc82c8e56bb47d71ee759305164f48013f7b2c4
what1s1ove <zubko.2007@gmail.com>
2021-02-28 15:36:40 +0200

    development: + handle error middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import express, { json, urlencoded } from 'express';
 import { ENV } from '~/common/enums';
 import { logger } from '~/services/services';
-import { setTraceId, logRequest } from '~/middlewares';
+import { setTraceId, logRequest, handleError } from '~/middlewares';
 import { DbConnectionError } from '~/exceptions';
 import { sequelize } from '~/data/db/connection';
 
@@ -22,15 +22,15 @@ app.use(logRequest);
 app.use(json());
 app.use(urlencoded({ extended: true }));
 
+app.use(handleError);
+
 app.use(express.static(join(__dirname, '../public')));
 app.use('*', (_req, res) => {
   return res.sendFile(join(__dirname, '../public', 'index.html'));
 });
 
 const server = app.listen(ENV.APP.SERVER_PORT, () => {
-  logger.log(
-    `Listening to connections on port — ${ENV.APP.SERVER_PORT}`,
-  );
+  logger.log(`Listening to connections on port — ${ENV.APP.SERVER_PORT}`);
 });
 
 export { server };
