9bbc88956e7de8b45b0d01bcc27c8b18cb8b29a7
Andrii Petrynchyn <petrynchyn@gmail.com>
2021-03-05 21:19:33 +0200

    HC-3: + add Sign Up Page

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { store } from 'store/store';
 import App from './app';
+import Registration from './containers/SignUpPage/SignUpPage';
+
 import './assets/styles/index.scss';
 
 render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <App />
+        <Route path="/sign-up" component={Registration} />
+        <Route exact path="/" component={App} />
       </Router>
     </Provider>
   </React.StrictMode>,
