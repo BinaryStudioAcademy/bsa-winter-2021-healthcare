6162b0e8ba0f0e1b473104c0ba105691d6d126b7
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-05 21:29:41 +0200

    HC-7 + add user interface and export it

---
+++ frontend/src/common/interfaces/app/user.interface.ts
@@ -0,0 +1,15 @@
+interface UserState {
+  id: string,
+  Applicant: string,
+  Birthday: string,
+  Phone: string,
+  Password: string,
+  Email: string,
+  ImagePath: string,
+  Geoposition: string,
+  Diagnosis: string,
+  CreatedAt: string,
+  UpdatedAt: string
+}
+
+export type { UserState };
