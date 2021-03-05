c631669ba6ca37e61f0a67189bd1f1b4ba093ac8
Yarikks <jasper18poil@gmail.com>
2021-03-05 23:04:59 +0200

    HC-5: * include auth apis to root api

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -1,14 +1,17 @@
 import { Router } from 'express';
 import { AppConfig } from '~/common/enums';
 import { initUserApi } from './user/user.api';
+import { initAuthApi } from './auth/auth.api';
 
 const apis = [initUserApi];
+const authApis = [initAuthApi];
 
 const initApi = (app: Router): Router => {
   const apiRouter = Router();
   app.use(AppConfig.API_V1_PREFIX, apiRouter);
 
   apis.forEach((api) => api(apiRouter));
+  authApis.forEach((api) => api(apiRouter));
 
   return apiRouter;
 };
