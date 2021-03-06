86582d1743f0564ab31cb28a7f7c7c30c5ade9ae
Yarikks <jasper18poil@gmail.com>
2021-03-06 15:50:39 +0200

    HC-5: * update return type

--- backend/src/helpers/string/create-token/create-token.helper.ts
+++ backend/src/helpers/string/create-token/create-token.helper.ts
@@ -2,4 +2,4 @@ import jwt from 'jsonwebtoken';
 import { secret, expiresIn } from '../../../../config/jwt.config';
 
 // TODO: define data type
-export const createToken = (data: any) => jwt.sign(data, secret, { expiresIn });
+export const createToken = (data: Record<string, unknown>): string => jwt.sign(data, secret, { expiresIn });
--- backend/src/services/auth/auth.service.ts
+++ backend/src/services/auth/auth.service.ts
@@ -1,6 +1,10 @@
 import { createToken } from '~/helpers';
 
-export const login = async ({ id }: { id: string }) => ({
+export const login = async ({
+  id,
+}: {
+  id: string;
+}): Promise<{ token: string }> => ({
   token: createToken({ id }),
   // TODO: user: await userRepository.getUserById(id)
 });
