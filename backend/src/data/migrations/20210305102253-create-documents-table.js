94930881fba287fd1febd8c0dd5ff3f3b4d6be02
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-06 16:46:02 +0200

    HC-11: + status column to documents table

--- backend/src/data/migrations/20210305102253-create-documents-table.js
+++ backend/src/data/migrations/20210305102253-create-documents-table.js
@@ -15,6 +15,10 @@ module.exports = {
           allowNull: false,
           type: Sequelize.STRING
         },
+        status: {
+          allowNull: false,
+          type: Sequelize.ENUM('verified', 'in_review')
+        },
         createdAt: Sequelize.DATE,
         updatedAt: Sequelize.DATE
       }, { transaction })
