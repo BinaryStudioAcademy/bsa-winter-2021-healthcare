b0c0f149cf1d464e98badf7ea533ec26a7201536
Yarikks <jasper18poil@gmail.com>
2021-03-06 13:59:33 +0200

    HC-5: * update services root

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,2 +1,10 @@
-export * from './http/http.service';
-export * from './auth-api/auth-api.service';
+import { Http } from './http/http.service';
+import { AuthApi } from './auth-api/auth-api.service';
+
+const http = new Http();
+
+const authApi = new AuthApi({
+  http,
+});
+
+export { http, authApi };
