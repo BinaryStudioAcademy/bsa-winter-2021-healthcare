5bb98ca3e690c6ff6a15bea258027408f40fb880
Yarikks <jasper18poil@gmail.com>
2021-03-07 18:30:35 +0200

    HC-5: * make use of enums, interfaces and helpers from shared

--- frontend/src/containers/SignInPage/index.tsx
+++ frontend/src/containers/SignInPage/index.tsx
@@ -1,36 +1,21 @@
 import * as React from 'react';
-import {useForm} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-// import { isValidEmail, isValidPassword } from 'healthcare-shared/helpers';
-
 import styles from './signin.module.scss';
-
-enum RegisterPayloadKey {
-  EMAIL = 'email',
-  PASSWORD = 'password',
-}
-interface IRegisterPayload {
-  [RegisterPayloadKey.EMAIL]: string;
-  [RegisterPayloadKey.PASSWORD]: string;
-}
-/* const DEFAULT_VALUES: IRegisterPayload = {
-  [RegisterPayloadKey.EMAIL]: '',
-  [RegisterPayloadKey.PASSWORD]: '',
-}; */
+import { IUserLoginPayload } from 'healthcare-shared/common/interfaces';
+import { LoginPayloadKey } from 'healthcare-shared/common/enums';
+import { loginSchema } from 'healthcare-shared/helpers';
 
 const SignInPage: React.FC = () => {
-  const validationSchema = yup.object().shape({
-      email: yup.string().email('Email is incorrect').required('Email is required'),
-      password: yup.string().length(6, 'min 6 chars').required('Password is required min six symbols')
-    });
-  const { register, handleSubmit, errors } = useForm<IRegisterPayload>({
-    resolver: yupResolver(validationSchema)
+  const { register, handleSubmit, errors } = useForm<IUserLoginPayload>({
+    resolver: yupResolver(loginSchema),
   });
-  const onSubmit=(formValues: IRegisterPayload) => {
+
+  const onSubmit = (formValues: IUserLoginPayload) => {
     console.log(formValues);
-  }
-    return (
+  };
+
+  return (
     <div className={styles.wrapper}>
       <div className={styles.formWrapper}>
         <h2>Sign In</h2>
@@ -38,25 +23,31 @@ const SignInPage: React.FC = () => {
           <div className={styles.email}>
             <label htmlFor="email">Email</label>
             <input
-              type={RegisterPayloadKey.EMAIL}
-              name={RegisterPayloadKey.EMAIL}
-              placeholder='mail@gmail.com'
+              type={LoginPayloadKey.EMAIL}
+              name={LoginPayloadKey.EMAIL}
+              placeholder="mail@gmail.com"
               ref={register}
             />
-            {errors.email && <span className={styles.errorSpan}>{errors.email.message}</span>}
+            {errors.email && (
+              <span className={styles.errorSpan}>{errors.email.message}</span>
+            )}
           </div>
           <div className={styles.password}>
             <label htmlFor="password">Password</label>
             <input
-              type={RegisterPayloadKey.PASSWORD}
-              name={RegisterPayloadKey.PASSWORD}
-              placeholder='******'
+              type={LoginPayloadKey.PASSWORD}
+              name={LoginPayloadKey.PASSWORD}
+              placeholder="******"
               ref={register}
             />
-            {errors.password && <span className={styles.errorSpan}>{errors.password.message}</span>}
+            {errors.password && (
+              <span className={styles.errorSpan}>
+                {errors.password.message}
+              </span>
+            )}
           </div>
           <div className={styles.submit}>
-            <button type='submit'>Sign In</button>
+            <button type="submit">Sign In</button>
           </div>
         </form>
       </div>
@@ -64,4 +55,3 @@ const SignInPage: React.FC = () => {
   );
 };
 export default SignInPage;
-
