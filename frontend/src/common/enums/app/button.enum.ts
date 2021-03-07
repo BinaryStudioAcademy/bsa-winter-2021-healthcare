1641a115a59647e0c480615697086594a9183c08
Dnzln <qvanoman@gmail.com>
2021-03-07 13:21:02 +0200

    HC-9: + add enums for input and buttons

---
+++ frontend/src/common/enums/app/button.enum.ts
@@ -0,0 +1,9 @@
+enum ButtonNames {
+  SIGN_IN = 'sign-in',
+  SIGN_IN_GOOGLE = 'sign-in-google',
+  APPOINTMENT = 'make-appointment',
+  COVID = 'covid-btn',
+  READ_ALL = 'read-all-reviews',
+}
+
+export { ButtonNames };
---
+++ frontend/src/common/enums/app/text-input.enum.ts
@@ -0,0 +1,10 @@
+enum TextInputNames {
+  NAME = 'name',
+  SURNAME = 'surname',
+  EMAIL = 'email',
+  PHONE = 'phone',
+  PASSWORD = 'password',
+  PASSWORD_REPEAT = 'password-repeat',
+}
+
+export { TextInputNames };
