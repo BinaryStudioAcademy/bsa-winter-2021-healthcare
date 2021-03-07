0e981e13a3db5879d5a4d66be7489cd42e2fe5d1
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-07 17:16:18 +0200

    HC-7 + add editUser to reducers

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { ReducerName } from 'common/enums';
-import { counterReducer } from './slices';
+import { counterReducer, editUserReducer } from './slices';
 
 const store = configureStore({
   reducer: {
+    [ReducerName.EDIT_USER]: editUserReducer,
     [ReducerName.USERS]: counterReducer,
     [ReducerName.COUNTER]: counterReducer,
   },
