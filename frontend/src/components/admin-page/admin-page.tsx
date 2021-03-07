aa81279de4870effbbf5a1f143b1b1f275fa1e52
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-07 18:43:05 +0200

    HC-7 * add EditUser component, and show him

--- frontend/src/components/admin-page/admin-page.tsx
+++ frontend/src/components/admin-page/admin-page.tsx
@@ -3,27 +3,27 @@ import { useSelector, useDispatch } from 'react-redux';
 import clsx from 'clsx';
 import { RootState } from 'common/types';
 import { UsersActionCreator } from 'store/slices';
-import * as config from './config/config.json';
 import styles from './styles.module.scss';
 import SearchAndFilterForm from './search-and-filter-form';
 import Users from './users';
+import EditUser from './edit-user';
 
 const AdminPage: React.FC = () => {
-  const { editUser } = useSelector(({ editUser }: RootState) => ({
-    editUser: editUser,
+  const { editUser } = useSelector(({ users }: RootState) => ({
+    editUser: users.editUser,
   }));
   console.log(editUser.edit);
   return (
     <>
-      {editUser.edit ?
-      <h2>EDIT</h2>
-      :
-      <div className={styles.container}>
-        <h2 className={styles.title}>Entries</h2>
-        <SearchAndFilterForm />
-        <Users />
-      </div>
-        }
+      {editUser.edit ? (
+        <EditUser />
+      ) : (
+        <div className={styles.container}>
+          <h2 className={styles.title}>Entries</h2>
+          <SearchAndFilterForm />
+          <Users />
+        </div>
+      )}
     </>
   );
 };
