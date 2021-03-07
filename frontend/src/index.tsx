f9c5c7c4f0ea5cba3a3f35587c5f5fe45755b14d
Andrii Petrynchyn <petrynchyn@gmail.com>
2021-03-07 17:08:45 +0200

    HC-3: * refactor components structure

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,10 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { store } from 'store/store';
+import { AppRoute } from 'common/enums';
 import App from './app';
-import Registration from './containers/signup-page/signup.page';
+import SignUp from './components/signup/signup';
+// import SignIn from './components/signup/signin';
 
 import './assets/styles/index.scss';
 
@@ -12,7 +14,8 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <Route path="/sign-up" component={Registration} />
+        {/* <Route path={"/" + AppRoute.SIGN_IN} component={SignIn} /> */}
+        <Route path={"/" + AppRoute.SIGN_UP} component={SignUp} />
         <Route exact path="/" component={App} />
       </Router>
     </Provider>
