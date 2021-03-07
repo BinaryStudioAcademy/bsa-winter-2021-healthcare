fd3bfe199e798871b16bc8c00bc96c12afc5d945
Yarikks <jasper18poil@gmail.com>
2021-03-07 18:15:16 +0200

    HC-5: * move create-token to token folder

--- backend/src/helpers/string/create-token/create-token.helper.ts
+++
@@ -1,5 +0,0 @@
-import jwt from 'jsonwebtoken';
-import { secret, expiresIn } from '../../../../config/jwt.config';
-
-// TODO: define data type
-export const createToken = (data: Record<string, unknown>): string => jwt.sign(data, secret, { expiresIn });
--- backend/src/helpers/string/index.ts
+++ backend/src/helpers/string/index.ts
@@ -1,2 +1,1 @@
 export * from './get-random-id/get-random-id.helper';
-export * from './create-token/create-token.helper';
