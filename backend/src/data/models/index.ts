0435d4ec31bf6332ff6836d2e9a1a25daabad3f7
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-08 15:25:47 +0200

    HC-22: + add associations to models

--- backend/src/data/models/index.ts
+++ backend/src/data/models/index.ts
@@ -6,6 +6,7 @@ import createAppointmentModel from './appointment';
 import createNotificationModel from './notification';
 import createMessageModel from './message';
 import createUserModel from './user';
+import createDiagnosisModel from './diagnosis';
 
 const ClinicModel = createClinicModel(sequelize);
 const DoctorModel = createDoctorModel(sequelize);
@@ -14,6 +15,31 @@ const AppointmentModel = createAppointmentModel(sequelize);
 const NotificationModel = createNotificationModel(sequelize);
 const MessageModel = createMessageModel(sequelize);
 const UserModel = createUserModel(sequelize);
+const DiagnosisModel = createDiagnosisModel(sequelize);
+
+UserModel.hasOne(DoctorModel);
+UserModel.hasMany(AppointmentModel);
+UserModel.hasMany(MessageModel);
+UserModel.hasMany(NotificationModel);
+UserModel.hasMany(DiagnosisModel);
+
+DoctorModel.hasMany(AppointmentModel);
+DoctorModel.belongsTo(ClinicModel);
+DoctorModel.belongsTo(DocumentModel);
+DoctorModel.belongsTo(UserModel);
+
+AppointmentModel.belongsTo(DoctorModel);
+AppointmentModel.belongsTo(UserModel);
+
+DocumentModel.hasOne(DoctorModel);
+
+ClinicModel.hasMany(DoctorModel);
+
+MessageModel.belongsTo(UserModel);
+
+NotificationModel.belongsTo(UserModel);
+
+DiagnosisModel.belongsTo(UserModel);
 
 export {
   ClinicModel,
