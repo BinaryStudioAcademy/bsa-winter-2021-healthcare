2d3f5729ca752d7d3c8ac5d80efb3d3e362a3340
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-03-08 10:36:38 +0200

    HC-3: * update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from 'store/store';
-import { AppRoute } from 'common/enums';
 import App from './app';
-import Sign from './components/sign/sign'
 
 import './assets/styles/index.scss';
 
@@ -13,8 +11,7 @@ render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
-        <Route path={[AppRoute.SIGN_UP]} component={Sign} />
-        <Route exact path="/" component={App} />
+        <App />
       </Router>
     </Provider>
   </React.StrictMode>,
