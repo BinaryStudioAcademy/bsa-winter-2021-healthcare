e7e6ef14e82a26658e8183af76c217c2d123e537
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-09 20:56:58 +0200

    HC-7 * add async reroute to admin-page to edit and set user functions

--- frontend/src/components/admin-page/admin-page.tsx
+++ frontend/src/components/admin-page/admin-page.tsx
@@ -1,27 +1,34 @@
 import * as React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { RootState } from 'common/types';
 import { AppRoute } from 'common/enums';
 import { UsersActionCreator } from 'store/slices';
 import styles from './styles.module.scss';
 import Users from './users';
 import EditUser from './edit-user';
-import { IRegisterPayload } from 'healthcare-shared';
+import { IRegisterPayload, IUser } from 'healthcare-shared';
 
 const AdminPage: React.FC = () => {
-
+  const history = useHistory();
   const { user } = useSelector(({ users }: RootState) => ({
     user: users.editUser,
   }));
 
   const { pathname } = useLocation();
   const dispatch = useDispatch();
-  const editUser = (data:IRegisterPayload) => {
-    dispatch(UsersActionCreator.editUser(data));
+  const editUser = (data: IRegisterPayload) => {
+    const editedUser: IUser = Object.assign({}, user, data)
+    dispatch(UsersActionCreator.editUser(editedUser));
+    setTimeout(()=>{
+      history.push(AppRoute.ADMIN_PAGE);
+    },0)
   }
-  const setUser = (data:IRegisterPayload) => {
-    dispatch(UsersActionCreator.addUser(data))
+  const setUser = (data: IRegisterPayload) => {
+    dispatch(UsersActionCreator.addUser(data));
+    setTimeout(()=>{
+      history.push(AppRoute.ADMIN_PAGE);
+    },0)
   }
 
   switch (pathname) {
