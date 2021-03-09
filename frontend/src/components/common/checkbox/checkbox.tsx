66cb76e054ed589841b47ecce3f1d4919bbc53c7
Dnzln <qvanoman@gmail.com>
2021-03-09 22:27:43 +0200

    HC9: * rebuild checkbox component

--- frontend/src/components/common/checkbox/checkbox.tsx
+++ frontend/src/components/common/checkbox/checkbox.tsx
@@ -1,37 +1,35 @@
+import clsx from 'clsx';
+import { InputChangeCallback } from 'common/types';
 import * as React from 'react';
 import styles from './styles.module.scss';
 
-interface CheckboxType {
-  options: Array<string>;
-  onChange(value: string): void;
+interface Props {
+  onChange: InputChangeCallback;
+  isChecked: boolean;
+  isDisabled: boolean;
+  hasError: boolean;
+  name: string;
+  label: string;
 }
 
-export const Checkbox: React.FC<CheckboxType> = ({ onChange, options }) => {
-  return (
-    <div>
-      {
-        options.map(typeOfDoctor => {
-          return (
-            <p className={styles.checkboxRow}
-              key={typeOfDoctor}>
-              <input
-                onChange={(e) => onChange(e.target.value)}
-                id={typeOfDoctor}
-                className={styles.checkboxInput}
-                type="checkbox"
-                name={typeOfDoctor}
-                value={typeOfDoctor.toLowerCase()}
-              />
-              <label
-                htmlFor={typeOfDoctor}
-                className={styles.checkboxLabel}
-                >
-                {typeOfDoctor}
-              </label>
-            </p>
-          );
-        })
-      }
-    </div>
-  );
-}
+const Checkbox: React.FC<Props> = ({ isChecked, isDisabled, hasError, name, label, onChange }) => (
+  <p className={clsx(styles.checkboxRow, hasError && styles.error)}>
+    <input
+      onChange={onChange}
+      checked={isChecked}
+      disabled={isDisabled}
+      name={name}
+      id={name}
+      className={styles.checkboxInput}
+      type="checkbox"
+    />
+    <label
+      htmlFor={name}
+      className={styles.checkboxLabel}
+      >
+      {label}
+    </label>
+  </p>
+);
+
+export default Checkbox;
