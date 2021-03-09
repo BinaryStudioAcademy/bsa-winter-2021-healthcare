184df850175585a4eb8ca88ae050a59163ec1b1f
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-09 13:34:47 +0200

    HC-7 + add function for checking type for table elem

--- frontend/src/components/admin-page/users.tsx
+++ frontend/src/components/admin-page/users.tsx
@@ -1,21 +1,38 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { AppRoute } from 'common/enums';
 import { RootState } from 'common/types';
-import { Column } from 'common/interfaces';
 import { UsersActionCreator } from 'store/slices';
+// import { Column } from 'common/interfaces';
 import Table from './table';
 
+//If type is not the string use this function
+// const checkIdentifierType = (identifier:string):Column =>{
+//     return {
+//       Header: identifier.charAt(0).toUpperCase() + identifier.slice(1),
+//       accessor: identifier,
+//       Cell : (props:any)=>{
+//         if(['birthdate','createdAt','updatedAt'].includes(identifier)){
+//           return props.cell.value.toLocaleDateString()
+//         }else{
+//           return props.cell.value.toString()
+//         }
+//       }
+//     };
+// }
+
 const Users: React.FC = () => {
+  const history = useHistory();
   const { AllUsers } = useSelector(({ users }: RootState) => ({
     AllUsers: users.users,
   }));
   const dispatch = useDispatch();
-
-  const columns: Column[] = Object.keys(AllUsers[0]).map((identifier) => {
+  const columns = Object.keys(AllUsers[0]).map((identifier) => {
     return {
       Header: identifier.charAt(0).toUpperCase() + identifier.slice(1),
       accessor: identifier,
-    };
+    }
   });
   columns.push({
     Header: 'Actions',
@@ -26,16 +43,20 @@ const Users: React.FC = () => {
     dispatch(UsersActionCreator.getUsers());
   }, []);
 
-  const editUserHandler = (id:string) => {
-    dispatch(UsersActionCreator.showEdit(id))
-    return id
+  const editUserHandler = (id: string) => {
+    history.push(AppRoute.EDIT_USER);
+    dispatch(UsersActionCreator.setEditUser(id));
   };
+  const createUserHandler = () =>{
+    history.push(AppRoute.CREATE_USER);
+  }
 
   return (
     <>
+      <button onClick={createUserHandler}>Add user</button>
       <Table columns={columns} data={AllUsers} edit={editUserHandler} />
     </>
   );
-}
+};
 
 export default Users;
