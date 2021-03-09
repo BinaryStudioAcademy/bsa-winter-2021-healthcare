139f7dc655ef2e2fcf350dfb74f504ff7735e513
Dnzln <qvanoman@gmail.com>
2021-03-09 22:25:41 +0200

    HC9: + add enum for input

---
+++ frontend/src/common/enums/app/input.enum.ts
@@ -0,0 +1,12 @@
+enum InputType {
+  TEXT = 'text',
+  EMAIL = 'email',
+  PHONE = 'tel',
+  FILE = 'file',
+  PASSWORD = 'password',
+  CHECKBOX = 'checkbox',
+  RADIO = 'radio',
+  SEARCH = 'search',
+}
+
+export { InputType };
