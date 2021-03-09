8a0de8b5feb72a7c60071861201e7bfddeb968bb
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-09 20:59:15 +0200

    HC-7 + add CRUD functional

--- frontend/src/services/user-api/user-api.services.ts
+++ frontend/src/services/user-api/user-api.services.ts
@@ -1,4 +1,8 @@
-import { ApiPath, UsersApiPath } from 'healthcare-shared/common/enums';
+import {
+  ApiPath,
+  ContentType,
+  UsersApiPath,
+} from 'healthcare-shared/common/enums';
 import { ENV } from 'common/enums';
 import { Http } from '../http/http.service';
 import { HttpMethod } from 'common/enums';
@@ -10,33 +14,51 @@ type Constructor = {
 
 class UserApi {
   #http: Http;
-  #apiPrefix:string|undefined;
+  #apiPrefix: string | undefined;
   constructor({ http }: Constructor) {
     this.#http = http;
     this.#apiPrefix = ENV.API_PATH;
   }
-    // public loginUser(payload: IUserLoginPayload): Promise<IUser> {
-    //   return this.#http.load(UsersApiPath, {
-    //     method: HttpMethod.POST,
-    //     payload,
-    //   });
-    // }
-    public registerUser(payload: IUser): Promise<IUser> {
-      console.log(payload)
-      return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}${UsersApiPath.ROOT}`, {
+  // public loginUser(payload: IUserLoginPayload): Promise<IUser> {
+  //   return this.#http.load(UsersApiPath, {
+  //     method: HttpMethod.POST,
+  //     payload,
+  //   });
+  // }
+  public registerUser(payload: IUser): Promise<IUser> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.USERS}${UsersApiPath.ROOT}`,
+      {
         method: HttpMethod.POST,
-        payload:JSON.stringify(payload),
-      });
-    }
-    // public getUser(id: IUser[UserKey.ID]): Promise<IUser> {
-    //   return this.#http.load(`${UsersApiPath}${id}`, {
-    //     method: HttpMethod.GET,
-    //   });
-    // }
-  public getUsers(): Promise<IUser>  {
-    return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}`, {
+        payload: { ...payload },
+        contentType: ContentType.JSON,
+      },
+    );
+  }
+  public editUser(id: string | undefined, payload: IUser): Promise<IUser> {
+    return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}/${id}`, {
+      method: HttpMethod.PUT,
+      payload: { ...payload },
+      contentType: ContentType.JSON,
+    });
+  }
+  public getUser(id: string): Promise<IUser> {
+    return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}/${id}`, {
       method: HttpMethod.GET,
     });
+  }
+  public deleteUser(id: string): Promise<IUser> {
+    return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}/${id}`, {
+      method: HttpMethod.DELETE,
+    });
+  }
+  public getUsers(): Promise<IUser[]> {
+    return this.#http.load(
+      `${this.#apiPrefix}${ApiPath.USERS}${UsersApiPath.ROOT}`,
+      {
+        method: HttpMethod.GET,
+      },
+    );
   }
 }
 export { UserApi };
