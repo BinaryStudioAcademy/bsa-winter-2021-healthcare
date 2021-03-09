017a19d04f50db837f787772c93bf8eaae5852ff
Dnzln <qvanoman@gmail.com>
2021-03-09 22:31:24 +0200

    HC9: * rebuild text input component

--- frontend/src/components/common/text-input/text-input.tsx
+++ frontend/src/components/common/text-input/text-input.tsx
@@ -1,73 +1,32 @@
 import clsx from 'clsx';
-import { TextInputNames } from 'common/enums';
+import { InputType } from 'common/enums';
+import { InputChangeCallback } from 'common/types';
 import * as React from 'react';
 import styles from './styles.module.scss';
 
-interface TextInputTypes {
-  [TextInputNames.NAME]: string;
-  [TextInputNames.SURNAME]: string;
-  [TextInputNames.EMAIL]: string;
-  [TextInputNames.PHONE]: string;
-  [TextInputNames.PASSWORD]: string;
-  [TextInputNames.PASSWORD_REPEAT]: string;
+interface Props {
+  type: InputType;
+  label: string;
+  name: string;
+  value: string;
+  hasError: boolean;
+  isDisabled: boolean;
+  onChange: InputChangeCallback;
 }
 
-const DEFAULT_VALUES: TextInputTypes = {
-  [TextInputNames.NAME]: '',
-  [TextInputNames.SURNAME]: '',
-  [TextInputNames.EMAIL]: '',
-  [TextInputNames.PHONE]: '',
-  [TextInputNames.PASSWORD]: '',
-  [TextInputNames.PASSWORD_REPEAT]: '',
-};
+const TextInput: React.FC<Props> = ({type, isDisabled, label, name, value, hasError, onChange}) => (
+  <label className={styles.label}>
+    <p className={styles.labelText}>{label}</p>
+    <input
+      className={clsx(styles.textInput, hasError && styles.error)}
+      disabled={isDisabled}
+      type={type}
+      name={name}
+      placeholder={name}
+      value={value}
+      onChange={onChange}
+    />
+  </label>
+);
 
-const INPUT_TYPES: TextInputTypes = {
-  [TextInputNames.NAME]: 'text',
-  [TextInputNames.SURNAME]: 'text',
-  [TextInputNames.EMAIL]: 'email',
-  [TextInputNames.PHONE]: 'phone',
-  [TextInputNames.PASSWORD]: 'password',
-  [TextInputNames.PASSWORD_REPEAT]: 'password',
-};
-
-interface TextInputProps {
-  filedName: keyof TextInputTypes;
-  isValid?: boolean | null;
-  onChange(value: string): void;
-}
-
-export const TextInput: React.FC<TextInputProps> = ({onChange, filedName, isValid='null'}) => {
-  const [
-    inputValues,
-    setInputValue,
-  ] = React.useState<TextInputTypes>(DEFAULT_VALUES);
-
-  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    const { value, name } = target;
-    setInputValue({
-      ...inputValues,
-      [name]: value,
-    });
-    onChange(value);
-  };
-
-  const validationStyles: string = (isValid === true)
-    ? styles.validField
-    : (isValid === false)
-    ? styles.invalidField
-    : '';
-
-  return (
-    <label>
-      <p className={styles.labelText}>{filedName}</p>
-      <input
-        className={clsx(styles.textInput, validationStyles)}
-        type={INPUT_TYPES[filedName]}
-        name={filedName}
-        placeholder={filedName}
-        value={inputValues[filedName]}
-        onChange={handleChange}
-      />
-    </label>
-  );
-}
+export default TextInput;
