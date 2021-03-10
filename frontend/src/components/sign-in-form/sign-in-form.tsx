6e301667c6d8c02912dd31ff2681498c816fe51c
Yarikks <jasper18poil@gmail.com>
2021-03-10 02:21:33 +0200

    HC-5: * make use of auth.slice

--- frontend/src/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/components/sign-in-form/sign-in-form.tsx
@@ -1,29 +1,39 @@
 import * as React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useDispatch, useSelector } from 'react-redux';
 import styles from './signin.module.scss';
 import { IUserLoginPayload } from 'common/interfaces';
 import { LoginPayloadKey } from 'common/enums';
 import { login as loginSchema } from 'validation-schemas';
 import { Link } from 'components/common';
 import { AppRoute } from 'common/enums';
+import { AuthActionCreator } from 'store/slices';
+import { RootState } from 'common/types';
 
 const SignInForm: React.FC = () => {
   const { register, handleSubmit, errors } = useForm<IUserLoginPayload>({
     resolver: yupResolver(loginSchema),
   });
+  const dispatch = useDispatch();
+
+  const { user } = useSelector(({ auth }: RootState) => ({
+    user: auth.user,
+  }));
 
   const handleFormSubmit = (formValues: IUserLoginPayload) => {
-    console.log(formValues);
+    dispatch(AuthActionCreator.login(formValues));
   };
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.formWrapper}>
         <h2>Sign In</h2>
-        <p>No account? <Link to={AppRoute.SIGN_UP}>Sign up</Link></p>
+        <p>
+          No account? <Link to={AppRoute.SIGN_UP}>Sign up</Link>
+        </p>
         <form onSubmit={handleSubmit(handleFormSubmit)}>
-        <Link to={AppRoute.ROOT}>Forgot password</Link>
+          <Link to={AppRoute.ROOT}>Forgot password</Link>
           <div className={styles.email}>
             <label htmlFor={LoginPayloadKey.EMAIL}>Email</label>
             <input
@@ -54,7 +64,7 @@ const SignInForm: React.FC = () => {
             <button type="submit">Sign In</button>
           </div>
         </form>
-
+        {user ? <div>{user.name}</div> : null}
       </div>
     </div>
   );
