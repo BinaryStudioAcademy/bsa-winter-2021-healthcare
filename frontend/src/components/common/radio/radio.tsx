1cc4d100e6b69cc8c03d498fe1d20636e90b324b
Dnzln <qvanoman@gmail.com>
2021-03-10 21:42:31 +0200

    HC-9: * change radio component to receive array of IOptions

--- frontend/src/components/common/radio/radio.tsx
+++ frontend/src/components/common/radio/radio.tsx
@@ -1,36 +1,45 @@
 import clsx from 'clsx';
+import { IOption } from 'common/interfaces';
 import { InputChangeCallback } from 'common/types';
 import * as React from 'react';
 import styles from './styles.module.scss';
 
 interface Props {
-  onChange: InputChangeCallback;
+  options: IOption<string>[];
   isChecked: boolean;
   isDisabled?: boolean;
   hasError?: boolean;
   name: string;
-  value: string;
-  label: string;
+  onChange: InputChangeCallback;
 }
 
-const Radio: React.FC<Props> = ({ isChecked, isDisabled, hasError, name, value, label, onChange }) => (
-  <label
-    className={clsx(styles.radioLabel,
-      hasError && styles.error,
-      isChecked && styles.isChecked)
+const Radio: React.FC<Props> = ({ options, isChecked, isDisabled, hasError, name, onChange }) => (
+  <div className={styles.radioContainer}>
+    {
+      options.map(option => {
+        return (
+          <label
+            key={option.value}
+            className={clsx(styles.radioLabel,
+              hasError && styles.error,
+              isChecked && styles.isChecked)
+            }
+          >
+            {option.label}
+            <input
+              onChange={onChange}
+              checked={isChecked}
+              disabled={isDisabled}
+              name={name}
+              value={option.value}
+              className={styles.radioInput}
+              type="radio"
+            />
+          </label>
+        )
+      })
     }
-  >
-    {label}
-    <input
-      onChange={onChange}
-      checked={isChecked}
-      disabled={isDisabled}
-      name={name}
-      value={value}
-      className={styles.radioInput}
-      type="radio"
-    />
-  </label>
+  </div>
 );
 
 export default Radio;
