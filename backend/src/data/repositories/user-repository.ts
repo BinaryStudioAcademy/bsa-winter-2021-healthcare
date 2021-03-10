0f5be9a36878a102a76132de5d6e11e131d58a25
Andrii Petrynchyn <petrynchyn@gmail.com>
2021-03-10 20:40:52 +0200

    HC-3: * user-repository

--- backend/src/data/repositories/user-repository.ts
+++ backend/src/data/repositories/user-repository.ts
@@ -1,21 +1,25 @@
 import { UserModel } from '../models';
-import { IUser } from '~/common/interfaces';
+import { IUser, IRegisterPayload } from '~/common/interfaces';
 
 class UserRepository {
   public getAll(): Promise<IUser[]> {
     return UserModel.findAll();
   }
+
   public getById(id: string): Promise<IUser | null> {
     return UserModel.findByPk(id);
   }
-  public createUser(user: IUser): Promise<IUser> {
+
+  public createUser(user: IRegisterPayload): Promise<IUser> {
     return UserModel.create(user);
   }
+
   public findByEmail(email: string): Promise<IUser | null> {
     return UserModel.findOne({
       where: { email },
     });
   }
+
   public async updateById(id: string, data: IUser): Promise<IUser[]> {
     const result = await UserModel.update(data, {
       where: { id },
