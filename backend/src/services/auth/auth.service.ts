17e50e43a32f8ba6ee4457c4d4bcbbf390caa23e
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-03-10 09:39:39 +0200

    HC-3: * update imports

--- backend/src/services/auth/auth.service.ts
+++ backend/src/services/auth/auth.service.ts
@@ -1,5 +1,4 @@
-import { IUser } from '~/common/interfaces/user';
-import { IRegisterPayload } from '~/common/interfaces'
+import { IRegisterPayload, IUser } from '~/common/interfaces'
 import { userRepository } from '~/data/repositories';
 import { getPasswordHash } from '~/helpers';
 
