9c01c6e4b1068547ad42c84aa3eb4b9f7b98d973
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-10 20:33:09 +0200

    HC-7 + add route to admin-page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { AppRoute } from 'common/enums';
+import AdminPage from 'components/admin-page/admin-page';
 import NotFound from 'components/not-found/not-found';
 
 const App: React.FC = () => (
   <Switch>
+    <Route path={AppRoute.ADMIN_PAGE} exact component={AdminPage}/>
     <Route path="*" exact component={NotFound} />
   </Switch>
 );
