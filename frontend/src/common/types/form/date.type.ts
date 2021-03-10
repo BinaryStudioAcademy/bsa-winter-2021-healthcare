e9d9550e4703357d6e86792392e96c1109a20ff0
Dnzln <qvanoman@gmail.com>
2021-03-10 15:56:40 +0200

    HC-9: * add types for dateInput

---
+++ frontend/src/common/types/form/date.type.ts
@@ -0,0 +1,4 @@
+type DatePayload = Date | null;
+type DateChangeEvent = (date: DatePayload) => void;
+
+export type {DatePayload, DateChangeEvent};
--- frontend/src/common/types/form/index.ts
+++ frontend/src/common/types/form/index.ts
@@ -1,3 +1,4 @@
 export * from './button.type';
 export * from './input.type';
 export * from './select.type';
+export * from './date.type';
