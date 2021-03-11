2af716b4cbb2f010db7731c0a33e8e750bbff2b0
Yarikks <jasper18poil@gmail.com>
2021-03-11 20:45:27 +0200

    HC-14: * add more routes to AppRoute

--- frontend/src/common/enums/app/app-route.enum.ts
+++ frontend/src/common/enums/app/app-route.enum.ts
@@ -2,6 +2,11 @@ enum AppRoute {
   ROOT = '/root',
   SIGN_IN = '/sign-in',
   SIGN_UP = '/sign-up',
+  DOCTOR = '/doctor',
+  CLINIC = '/clinic',
+  MY_PROFILE = '/my-profile',
+  MY_CALENDAR = '/my-calendar',
+  MY_CHATS = '/my-chats',
 }
 
 export { AppRoute };
