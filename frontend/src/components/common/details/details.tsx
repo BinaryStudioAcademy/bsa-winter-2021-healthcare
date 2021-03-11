cf9aa2b48ff15439338dbf4f86c5f37b426b5a05
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-03-11 22:15:52 +0200

    HC-15: + common component details

---
+++ frontend/src/components/common/details/details.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.scss';
+
+interface Props {
+  icon: string;
+  title: string;
+  children: React.ReactNode;
+}
+
+const Details: React.FC<Props> = ({ icon, title, children }) => {
+  return (
+    <details className={clsx(styles.commonFilter)}>
+      <summary className={clsx(styles.filterInfo)}>
+        <img src={icon} loading="lazy" alt="Filter icon."/>
+        <div className={clsx(styles.filterName)}>{title}</div>
+      </summary>
+      {children}
+    </details>
+  )
+}
+
+export default Details;
