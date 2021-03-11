0605050bb4b570e607713e0306cb25ae69b8c21c
Yarikks <jasper18poil@gmail.com>
2021-03-11 20:52:05 +0200

    HC-14: + add header copmonent

---
+++ frontend/src/components/common/header/header.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import iconLogo from 'assets/images/icon-logo.svg';
+import { Button, Link } from '../index';
+import {
+  AppRoute,
+  ButtonColor,
+  ButtonIcon,
+  ButtonStyleType,
+  ButtonType,
+} from 'common/enums';
+import { Props, useVisible } from './common';
+import styles from './header.module.scss';
+
+const Header: React.FC<Props> = ({ user }) => {
+  const { ref, isVisible, setIsVisible } = useVisible(false);
+
+  const handleSearchClick = () => {
+    // TODO: add search logic
+  };
+
+  const handleNotificationsClick = () => {
+    // TODO: add notification logic
+  };
+
+  const toggleShowMenu = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.logoSection}>
+        <Link to={AppRoute.ROOT}>
+          <img src={iconLogo} loading="lazy" alt="HealthCare Logo" />
+          <div className={styles.logoLabel}>HealthCare</div>
+        </Link>
+      </div>
+      <div className={styles.pagesSection}>
+        <Link to={AppRoute.DOCTOR}>Doctor</Link>
+        <Link to={AppRoute.CLINIC}>Clinic</Link>
+        <Link to={AppRoute.MY_PROFILE}>My Profile</Link>
+        <Link to={AppRoute.MY_CALENDAR}>My Calendar</Link>
+        <Link to={AppRoute.MY_CHATS}>My Chats</Link>
+      </div>
+      <div className={styles.userSection}>
+        <Button
+          type={ButtonType.BUTTON}
+          styleType={ButtonStyleType.MEDIUM_ROUND}
+          color={ButtonColor.GRAY_LIGHT}
+          label=""
+          onClick={handleSearchClick}
+          hasHiddenLabel={true}
+          icon={ButtonIcon.SEARCH}
+        />
+        <Button
+          type={ButtonType.BUTTON}
+          styleType={ButtonStyleType.MEDIUM_ROUND}
+          color={ButtonColor.GRAY_LIGHT}
+          label=""
+          onClick={handleNotificationsClick}
+          hasHiddenLabel={true}
+          icon={ButtonIcon.BELL}
+        />
+        <img className={styles.avatar} src={user.imagePath} />
+        <div className={styles.userInfo}>{user.name}</div>
+        <Button
+          type={ButtonType.BUTTON}
+          styleType={ButtonStyleType.MEDIUM_ROUND}
+          color={ButtonColor.GRAY_LIGHT}
+          label=""
+          onClick={toggleShowMenu}
+          hasHiddenLabel={true}
+          icon={ButtonIcon.EXPAND}
+        />
+        {isVisible ? (
+          <div className={styles.dropdownMenu} ref={ref}>
+            <div className={styles.userName}>{user.name}</div>
+            <Button
+              type={ButtonType.BUTTON}
+              styleType={ButtonStyleType.WITHOUT_BORDER}
+              color={ButtonColor.GRAY_LIGHT}
+              label="Logout"
+              onClick={toggleShowMenu}
+              hasHiddenLabel={false}
+            />
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
