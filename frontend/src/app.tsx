f459ad6671b93f604d63f2e10571c6f262d05564
ReznikYuriy <reznik333@gmail.com>
2021-03-12 13:33:01 +0200

    HC-21: * update css (add blue bubbles in <li> tag

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -3,11 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 import { AppRoute } from 'common/enums';
 import Sign from 'components/sign/sign'
 import NotFound from 'components/not-found/not-found';
+import DoctorDetails from 'components/doctor-details/doctor-details';
 
 const App: React.FC = () => (
   <Switch>
     <Route path={[AppRoute.SIGN_IN, AppRoute.SIGN_UP]} component={Sign}/>
+    <Route path="/details" component={DoctorDetails} />
     <Route path="*" exact component={NotFound} />
+
   </Switch>
 );
 
