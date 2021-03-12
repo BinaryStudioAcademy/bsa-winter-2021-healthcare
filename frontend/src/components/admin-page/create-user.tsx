fb153f14455bff1acd7766d8e06f4dec0a06bbec
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-12 16:46:44 +0200

    HC-7 * edit defoult value for state and fixed mistakes

--- frontend/src/components/admin-page/create-user.tsx
+++ frontend/src/components/admin-page/create-user.tsx
@@ -2,28 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import clsx from 'clsx';
-import { RegisterPayloadKey } from 'common/enums';
+import { RegisterPayloadKey, UserSex, UserType } from 'common/enums';
 import {
   IRegisterPayload,
   IUser,
 } from 'common/interfaces';
-import { validationUserSchema } from 'helpers';
+import { userRegister as validationUserSchema } from 'helpers';
 import styles from './styles.module.scss';
 
-interface IProps{
-  user?:IUser,
-  func:(data:IRegisterPayload)=>void,
-  hideForm: ()=>void,
+interface IProps {
+  user?: IUser,
+  func: (data: IRegisterPayload) => void,
+  hideForm: () => void,
 }
 
 const DEFAULT_VALUES: IRegisterPayload = {
   [RegisterPayloadKey.NAME]: '',
   [RegisterPayloadKey.SURNAME]: '',
+  [RegisterPayloadKey.SEX]: UserSex.MALE,
+  [RegisterPayloadKey.BIRTH_DATE]: '',
   [RegisterPayloadKey.EMAIL]: '',
   [RegisterPayloadKey.PASSWORD]: '',
   [RegisterPayloadKey.RETYPE_PASSWORD]: '',
   [RegisterPayloadKey.PHONE]: '',
-  [RegisterPayloadKey.IS_STAFF]: false,
+  [RegisterPayloadKey.TYPE]: UserType.PATIENT,
+  [RegisterPayloadKey.IMAGE_PATH]: 'https://www.pikpng.com/pngl/b/80-805523_default-avatar-svg-png-icon-free-download-264157.png'
 };
 
 
@@ -43,111 +46,117 @@ const CreateUser: React.FC<IProps> = ({ user, func, hideForm }) => {
 
   useEffect(() => {
     user ? setUserState({ ...userState, ...user }) : null;
-  },[]);
+  }, []);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.editForm}>
+      <h2 className={styles.title}>Add User</h2>
+
       <div className={styles.inputElements}>
-        <div className={styles.inputRow}>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            placeholder="Name"
-            value={userState.name}
-            name={RegisterPayloadKey.NAME}
-            ref={register}
-            onChange={(e) => inputHandler(e)}
-          />
-          {errors.name && (
-            <span className={styles.errorSpan}>{errors.name.message}</span>
-          )}
-        </div>
+        <label htmlFor={RegisterPayloadKey.NAME}>Name</label>
+        <input
+          type="text"
+          placeholder="Name"
+          name={RegisterPayloadKey.NAME}
+          ref={register}
+        />
+        {errors.name && <span className={styles.errorSpan}>{errors.name.message}</span>}
+      </div>
 
-        <div className={clsx(styles.surname, styles.inputRow)}>
-          <label htmlFor="surname">Surname</label>
-          <input
-            type="text"
-            placeholder="Surname"
-            value={userState.surname}
-            name={RegisterPayloadKey.SURNAME}
-            ref={register}
-            onChange={(e) => inputHandler(e)}
-          />
-          {errors.surname && (
-            <span className={styles.errorSpan}>{errors.surname.message}</span>
-          )}
-        </div>
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.SURNAME}>Surname</label>
+        <input
+          type="text"
+          placeholder="Surname"
+          name={RegisterPayloadKey.SURNAME}
+          ref={register}
+        />
+        {errors.surname && <span className={styles.errorSpan}>{errors.surname.message}</span>}
+      </div>
 
-        <div className={clsx(styles.email, styles.inputRow)}>
-          <label htmlFor="email">E-mail</label>
-          <input
-            type="email"
-            placeholder="E-mail"
-            value={userState.email}
-            name={RegisterPayloadKey.EMAIL}
-            ref={register}
-            onChange={(e) => inputHandler(e)}
-          />
-          {errors.email && (
-            <span className={styles.errorSpan}>{errors.email.message}</span>
-          )}
-        </div>
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.SEX}>Gender</label>
+        <select name={RegisterPayloadKey.SEX} ref={register}>
+          <option value={UserSex.FEMALE}>female</option>
+          <option value={UserSex.MALE}>male</option>
+        </select>
+      </div>
 
-        <div className={clsx(styles.password, styles.inputRow)}>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            placeholder="Password"
-            value={userState.password}
-            name={RegisterPayloadKey.PASSWORD}
-            ref={register}
-            onChange={(e) => inputHandler(e)}
-          />
-          {errors.password && (
-            <span className={styles.errorSpan}>{errors.password.message}</span>
-          )}
-        </div>
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.BIRTH_DATE}>Birthday</label>
+        <input type="date" name={RegisterPayloadKey.BIRTH_DATE} ref={register} />
+      </div>
 
-        <div className={clsx(styles.password, styles.inputRow)}>
-          <label htmlFor="password">Retype Password</label>
-          <input
-            type="password"
-            placeholder="Retype password"
-            value={userState.retypePassword}
-            name={RegisterPayloadKey.RETYPE_PASSWORD}
-            ref={register}
-            onChange={(e) => inputHandler(e)}
-          />
-          {errors.retypePassword && (
-            <span className={styles.errorSpan}>
-              {errors.retypePassword.message}
-            </span>
-          )}
-        </div>
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.EMAIL}>E-mail</label>
+        <input
+          type="email"
+          placeholder="E-mail"
+          name={RegisterPayloadKey.EMAIL}
+          ref={register}
+        />
+        {errors.email && <span className={styles.errorSpan}>{errors.email.message}</span>}
+      </div>
 
-        <div className={clsx(styles.phone, styles.inputRow)}>
-          <label htmlFor="phone">Phone</label>
-          <input
-            type="tel"
-            placeholder="Phone"
-            value={userState.phone}
-            name={RegisterPayloadKey.PHONE}
-            ref={register}
-            onChange={(e) => inputHandler(e)}
-          />
-          {errors.phone && (
-            <span className={styles.errorSpan}>{errors.phone.message}</span>
-          )}
-        </div>
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.PASSWORD}>Password</label>
+        <input
+          type="password"
+          placeholder="Password"
+          name={RegisterPayloadKey.PASSWORD}
+          ref={register}
+        />
+        {errors.password && <span className={styles.errorSpan}>{errors.password.message}</span>}
+      </div>
 
-        <div className={styles.checkBox}>
-          <input
-            type="checkbox"
-            name={RegisterPayloadKey.IS_STAFF}
-            ref={register}
-          />
-          <label htmlFor="isStaff">Doctor/Nurse</label>
-        </div>
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.RETYPE_PASSWORD}>Retype Password</label>
+        <input
+          type="password"
+          placeholder="Retype password"
+          name={RegisterPayloadKey.RETYPE_PASSWORD}
+          ref={register}
+        />
+        {errors.retypePassword && <span className={styles.errorSpan}>{errors.retypePassword.message}</span>}
+      </div>
+
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.PHONE}>Phone</label>
+        <input
+          type="tel"
+          placeholder="Phone"
+          name={RegisterPayloadKey.PHONE}
+          ref={register}
+        />
+        {errors.phone && <span className={styles.errorSpan}>{errors.phone.message}</span>}
+      </div>
+
+      <div className={styles.inputElements}>
+        <label htmlFor={RegisterPayloadKey.TYPE}>Status</label>
+        <select name={RegisterPayloadKey.TYPE} ref={register}>
+          <option value={UserType.PATIENT}>Patient</option>
+          <option value={UserType.DOCTOR}>Doctor/Nurse</option>
+        </select>
+      </div>
+
+      {/* <div className={styles.inputElements}>
+  <input type="button" value="Upload documents" />
+  <label>
+    Upload file:
+    <input type="file" multiple />
+  </label>
+  <span>file1.pdf</span>
+  <span>file2.jpg</span>
+</div> */}
+
+      <div className={styles.inputElements}>
+        <input
+          type="hidden"
+          name={RegisterPayloadKey.IMAGE_PATH}
+          ref={register}
+          value={userState.imagePath}
+        />
+        {errors.imagePath && <span className={styles.errorSpan}>{errors.imagePath.message}</span>}
       </div>
 
       <button type="submit">Save</button>
