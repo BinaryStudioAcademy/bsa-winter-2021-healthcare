42062f877dc26e93d0324f71c258656be67fdfac
Yarikks <jasper18poil@gmail.com>
2021-03-12 20:20:41 +0200

    HC-14: * change button & link components, use link in button

--- frontend/src/components/common/button/button.tsx
+++ frontend/src/components/common/button/button.tsx
@@ -1,7 +1,8 @@
 import clsx from 'clsx';
-import { ButtonColor, ButtonIcon, ButtonStyleType, ButtonType } from 'common/enums';
+import { AppRoute, ButtonColor, ButtonIcon, ButtonStyleType, ButtonType } from 'common/enums';
 import { ButtonClickCallback } from 'common/types';
 import * as React from 'react';
+import Link from '../link/link';
 import styles from './styles.module.scss';
 
 interface Props {
@@ -13,17 +14,32 @@ interface Props {
   hasHiddenLabel: boolean;
   isDisabled?: boolean;
   icon?: ButtonIcon;
+  href?: AppRoute;
 }
 
-const Button: React.FC<Props> = ({ type = ButtonType.BUTTON, styleType, color, label, isDisabled, hasHiddenLabel, icon, onClick }) => (
-  <button
-    className={clsx(styles.btn, styles[styleType], styles[color], icon && styles[icon])}
-    type={type}
-    disabled={isDisabled}
-    onClick={onClick}>
-    {hasHiddenLabel ? <span className="visually-hidden">{label}</span> : label}
-    {icon && <span className={clsx(styles.buttonIcon, styles[icon])}></span>}
-  </button>
-);
+const Button: React.FC<Props> = ({ type = ButtonType.BUTTON, styleType, color, label, isDisabled, hasHiddenLabel, icon, href, onClick }) => {
+  return (
+    <button
+      className={clsx(styles.btn, styles[styleType], styles[color], icon && styles[icon])}
+      type={type}
+      disabled={isDisabled}
+      onClick={onClick}>
+      { href
+        ? (
+          <Link to={href}>
+            {hasHiddenLabel ? <span className="visually-hidden">{label}</span> : label}
+            {icon && <span className={clsx(styles.buttonIcon, styles[icon])}></span>}
+          </Link>
+        )
+        : (
+          <div>
+            {hasHiddenLabel ? <span className="visually-hidden">{label}</span> : label}
+            {icon && <span className={clsx(styles.buttonIcon, styles[icon])}></span>}
+          </div>
+        )
+      }
+    </button>
+  );
+}
 
 export default Button;
--- frontend/src/components/common/link/link.tsx
+++ frontend/src/components/common/link/link.tsx
@@ -3,8 +3,8 @@ import { NavLink as AppLink } from 'react-router-dom';
 import { Props } from './common/types';
 import styles from './link.module.scss';
 
-const Link: React.FC<Props> = ({ children, to }) => (
-  <AppLink to={to} activeClassName={styles.active}>
+const Link: React.FC<Props> = ({ children, to, activeClass, className }) => (
+  <AppLink to={to} className={className ?? styles.link} activeClassName={activeClass ?? styles.active}>
     {children}
   </AppLink>
 );
