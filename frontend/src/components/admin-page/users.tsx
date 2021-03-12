3827a9cf6543da200f27cf09095d3a8186eceb89
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-12 11:16:59 +0200

    HC-7 * edit date identifiers to enums

--- frontend/src/components/admin-page/users.tsx
+++ frontend/src/components/admin-page/users.tsx
@@ -1,58 +1,58 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { AppRoute } from 'common/enums';
 import { RootState } from 'common/types';
 import { UsersActionCreator } from 'store/slices';
-import { Column } from 'common/interfaces';
-import Table from './table';
-
+import { Column, IUser } from 'common/interfaces';
+import Table from '../common/table/table';
+import ActionsButton from './actions-button';
+import { DateIdentifiers } from 'common/enums';
 
 const checkIdentifierType = (identifier:string):Column =>{
     return {
       Header: identifier.charAt(0).toUpperCase() + identifier.slice(1),
       accessor: identifier,
-      Cell : (props:any)=>{
-        if(['birthdate','createdAt','updatedAt'].includes(identifier)){
-          return props.cell.value.slice(0,10).replaceAll('-', '/')
+      Cell : ({value}:any)=>{
+        const dates:string[] = [DateIdentifiers.birthdate,DateIdentifiers.createdAt,DateIdentifiers.updatedAt];
+        if(dates.includes(identifier)){
+          return value.toString().slice(0,10).replaceAll('-', '/')
         }else{
-          return props.cell.value ? props.cell.value.toString() : ''
+          return value ? value.toString() : ''
         }
       }
     };
 }
 
-const Users: React.FC = () => {
+interface IProps{
+  deleteUser: (id:string) => void,
+  showForm: (user?:IUser) => void
+}
+
+const Users: React.FC<IProps> = ({ showForm,deleteUser }) => {
   const { AllUsers } = useSelector(({ users }: RootState) => ({
     AllUsers: users.users,
   }));
   const dispatch = useDispatch();
   const columns = Object.keys(AllUsers[0]).map((identifier) => {
-    return checkIdentifierType(identifier)
+    return checkIdentifierType(identifier);
   });
   columns.push({
     Header: 'Actions',
     accessor: 'actions',
+    Cell: function func ({row}:any){
+      return <ActionsButton value={row.original} edit={showForm} deleteUser={deleteUser}/>
+    }
   });
 
   useEffect(() => {
     dispatch(UsersActionCreator.getUsers());
   }, []);
 
-  const editUserHandler = (id: string) => {
-    dispatch(UsersActionCreator.setEditUser(id));
-  };
-  const createUserHandler = () =>{
-    console.log("CREATE");
-  }
-  const deleteUser = (id:string) =>{
-    dispatch(UsersActionCreator.deleteUser(id));
-  }
+  const showFormHandler = ():void => showForm();
 
   return (
     <>
-      <button onClick={createUserHandler}>Add user</button>
-      <Table columns={columns} data={AllUsers} edit={editUserHandler} deleteUser={deleteUser} />
+      <button onClick={showFormHandler}>Add user</button>
+      <Table columns={columns} data={AllUsers}/>
     </>
   );
 };
