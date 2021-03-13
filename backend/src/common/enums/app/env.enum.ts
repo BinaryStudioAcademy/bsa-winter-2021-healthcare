b8e8b810eb194e866f97d0141c637701d4606aa7
what1s1ove <zubko.2007@gmail.com>
2021-03-13 16:57:33 +0200

    main: * server port env

--- backend/src/common/enums/app/env.enum.ts
+++ backend/src/common/enums/app/env.enum.ts
@@ -1,11 +1,11 @@
 import { AppEnvironment } from '~/common/enums/app/app-environment.enum';
 
-const { NODE_ENV, APP_SERVER_PORT, SECRET_KEY } = process.env;
+const { NODE_ENV, PORT, SECRET_KEY } = process.env;
 
 const ENV = {
   APP: {
     NODE_ENV: NODE_ENV as AppEnvironment,
-    SERVER_PORT: APP_SERVER_PORT,
+    SERVER_PORT: PORT,
     SECRET: SECRET_KEY ?? '',
   },
 };
