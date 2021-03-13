c4bf1a8a0a92a9f5da1cf36df10873316e0731ba
Dnzln <qvanoman@gmail.com>
2021-03-13 23:36:16 +0200

    HC24: * add error handling to auth action

--- frontend/src/store/slices/auth/auth.slice.ts
+++ frontend/src/store/slices/auth/auth.slice.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ReducerName, DataStatus, StorageKey } from 'common/enums';
 import { IUser, IUserLoginPayload, IRegisterPayload } from 'common/interfaces';
-import { authApi, storage } from 'services';
+import { authApi, notificationService, storage } from 'services';
 import { LoginResponse } from 'common/types/responses';
 
 
@@ -18,18 +18,28 @@ const initialState: AuthState = {
 const login = createAsyncThunk(
   'auth/login',
   async (userData: IUserLoginPayload) => {
-    const { token, user }: LoginResponse = await authApi.loginUser(userData);
-    storage.setItem(StorageKey.TOKEN, token);
-    return user;
+    try {
+      const { token, user }: LoginResponse = await authApi.loginUser(userData);
+      storage.setItem(StorageKey.TOKEN, token);
+      return user;
+    } catch(error) {
+      notificationService.error(`Error ${error.status}`, error.message);
+      throw error;
+    }
   },
 );
 
 const registration = createAsyncThunk(
   'auth/registration',
   async (userData: IRegisterPayload) => {
-    const { token, user } = await authApi.registrationUser(userData);
-    storage.setItem(StorageKey.TOKEN, token);
-    return user;
+    try {
+      const { token, user } = await authApi.registrationUser(userData);
+      storage.setItem(StorageKey.TOKEN, token);
+      return user;
+    } catch(error) {
+      notificationService.error(`Error ${error.status}`, error.message);
+      throw error;
+    }
   },
 );
 
