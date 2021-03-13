ec3af818a55ed5d51fd0c184578d8ce7971666b9
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-13 22:31:40 +0200

    HC-28: + migration to create users_permissions table

---
+++ backend/src/data/migrations/20210313161337-create-users_permissions-table.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(transaction => Promise.all([
+      queryInterface.createTable('users_permissions', {
+        userId: {
+          type: Sequelize.UUID,
+          references: {
+            model: 'users',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL'
+        },
+        permissionId: {
+          type: Sequelize.UUID,
+          references: {
+            model: 'permissions',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL'
+        }
+      }, { transaction })
+    ])),
+  down: async queryInterface => queryInterface.sequelize
+    .transaction(transaction => Promise.all([
+      queryInterface.dropTable('users_permissions', { transaction })
+    ]))
+};
