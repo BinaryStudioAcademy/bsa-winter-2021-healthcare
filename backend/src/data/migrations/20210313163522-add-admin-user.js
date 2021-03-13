36668beac7fb65c08d6da27d04d6976ee6ac2e54
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-13 22:32:47 +0200

    HC-28: + admin user to the db with all permissions

---
+++ backend/src/data/migrations/20210313163522-add-admin-user.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async queryInterface =>
+    queryInterface.sequelize.transaction(transaction => Promise.all([
+      queryInterface.bulkInsert('users', [
+        {
+          name: 'admin',
+          surname: 'admin',
+          birthdate: new Date(),
+          sex: 'female',
+          type: 'doctor',
+          phone: '+380900000000',
+          email: 'admin@admin.com',
+          password: 'admin',
+          imagePath: 'https://www.pikpng.com/pngl/b/80-805523_default-avatar-svg-png-icon-free-download-264157.png',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ], { transaction, returning: true })
+        .then(adminData => {
+          queryInterface.sequelize.query('SELECT * FROM permissions')
+            .then(data => queryInterface.bulkInsert('users_permissions',
+              data[0].map(permission => {
+                return {
+                  userId: adminData[0].id,
+                  permissionId: permission.id
+                }
+              })))
+        })
+    ])),
+  down: async queryInterface => queryInterface.sequelize
+    .transaction(transaction => Promise.all([
+      queryInterface.bulkDelete('users', null, { transaction }),
+      queryInterface.bulkDelete('users_permissions', null, { transaction })
+    ]))
+};
