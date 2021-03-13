2c8fe2b05a8a0cb9ad8db5756bae979ecee8cefb
Dnzln <qvanoman@gmail.com>
2021-03-13 19:15:52 +0200

    HC24: * fix package import order

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
-import { Toaster } from 'components/common';
 import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { store } from 'store/store';
 import App from './app';
+import { Toaster } from 'components/common';
 
 import './assets/styles/index.scss';
 
--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { reducer as toastrReducer } from 'react-redux-toastr';
 import { ReducerName } from 'common/enums';
 import { counterReducer, authReducer } from './slices';
-import { reducer as toastrReducer } from 'react-redux-toastr';
 
 const store = configureStore({
   reducer: {
