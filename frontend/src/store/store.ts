8e593ec1753366d9e3bd4cb768736979da6a0281
Dnzln <qvanoman@gmail.com>
2021-03-13 18:19:36 +0200

    HC24: * add toaster reducer

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { ReducerName } from 'common/enums';
 import { counterReducer, authReducer } from './slices';
+import { reducer as toastrReducer } from 'react-redux-toastr';
 
 const store = configureStore({
   reducer: {
     [ReducerName.COUNTER]: counterReducer,
     [ReducerName.AUTH]: authReducer,
+    [ReducerName.TOASTR]: toastrReducer,
   },
 });
 
