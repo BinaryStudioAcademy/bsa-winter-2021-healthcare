104c364f1bedbf97c09e4385205df52a9ea6d259
ReznikYuriy <reznik333@gmail.com>
2021-03-13 22:26:00 +0200

    HC-21: * update UsersApiPath (add details/:id route)

--- shared/src/common/enums/api/users-api-path.enum.ts
+++ shared/src/common/enums/api/users-api-path.enum.ts
@@ -2,7 +2,8 @@ enum UsersApiPath {
   ROOT = '/',
   $ID = '/:id',
   TYPE = '/type',
-  TYPE_$TYPE = '/type/:type'
+  TYPE_$TYPE = '/type/:type',
+  DETAILS_$ID = '/details/:id'
 }
 
 export { UsersApiPath };
