dc9c6bdd8d63b687bfd4140d58e6d40d27c656cd
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-14 13:46:51 +0200

    HC-7 - remove password and retype password

--- shared/src/validation-schemas/edit-user/edit-user-schema.helper.ts
+++ shared/src/validation-schemas/edit-user/edit-user-schema.helper.ts
@@ -7,8 +7,6 @@ const validationEditUser = yup.object().shape({
   [EditUserPayloadKey.SEX]: yup.mixed<UserSex>().oneOf(Object.values(UserSex)).required(RegisterValidationMessage.SEX_REQUIRED),
   [EditUserPayloadKey.BIRTHDATE]: yup.date().required(RegisterValidationMessage.BIRTH_DATE_REQUIRED),
   [EditUserPayloadKey.EMAIL]: yup.string().required(RegisterValidationMessage.EMAIL_REQUIRED).email(RegisterValidationMessage.EMAIL_INCORRECT),
-  [EditUserPayloadKey.PASSWORD]: yup.string().required(RegisterValidationMessage.PASSWORD_REQUIRED).min(RegisterValidationRule.PASSWORD_MIN_LENGTH, RegisterValidationMessage.PASSWORD_MIN_LENGTH),
-  [EditUserPayloadKey.RETYPE_PASSWORD]: yup.string().oneOf([yup.ref(EditUserPayloadKey.PASSWORD), null], RegisterValidationMessage.PASSWORD_REPEAT_MATCH),
   [EditUserPayloadKey.PHONE]: yup.string().required(RegisterValidationMessage.PHONE_REQUIRED),
   [EditUserPayloadKey.TYPE]: yup.mixed<UserType>().oneOf(Object.values(UserType)).required(),
 });
