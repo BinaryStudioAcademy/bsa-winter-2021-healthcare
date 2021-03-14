c0a684b494c3abbe41997356392fceca15466ce5
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-14 11:49:56 +0200

    HC-28: + password hash to admin migration

--- backend/src/data/migrations/20210313163522-add-admin-user.js
+++ backend/src/data/migrations/20210313163522-add-admin-user.js
@@ -1,33 +1,36 @@
 'use strict';
+import { getPasswordHash } from '~/helpers';
 
 module.exports = {
   up: async queryInterface =>
     queryInterface.sequelize.transaction(transaction => Promise.all([
-      queryInterface.bulkInsert('users', [
-        {
-          name: 'admin',
-          surname: 'admin',
-          birthdate: new Date(),
-          sex: 'female',
-          type: 'doctor',
-          phone: '+380900000000',
-          email: 'admin@admin.com',
-          password: 'admin',
-          imagePath: 'https://www.pikpng.com/pngl/b/80-805523_default-avatar-svg-png-icon-free-download-264157.png',
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ], { transaction, returning: true })
-        .then(adminData => {
-          queryInterface.sequelize.query('SELECT * FROM permissions')
-            .then(data => queryInterface.bulkInsert('users_permissions',
-              data[0].map(permission => {
-                return {
-                  userId: adminData[0].id,
-                  permissionId: permission.id
-                }
-              })))
-        })
+      getPasswordHash('Pa55word').then(passwordHash => {
+        queryInterface.bulkInsert('users', [
+          {
+            name: 'admin',
+            surname: 'admin',
+            birthdate: new Date(),
+            sex: 'female',
+            type: 'doctor',
+            phone: '+380900000000',
+            email: 'admin@admin.com',
+            password: passwordHash,
+            imagePath: 'https://www.pikpng.com/pngl/b/80-805523_default-avatar-svg-png-icon-free-download-264157.png',
+            createdAt: new Date(),
+            updatedAt: new Date()
+          }
+        ], { transaction, returning: true })
+          .then(adminData => {
+            queryInterface.sequelize.query('SELECT * FROM permissions')
+              .then(data => queryInterface.bulkInsert('users_permissions',
+                data[0].map(permission => {
+                  return {
+                    userId: adminData[0].id,
+                    permissionId: permission.id
+                  }
+                })))
+          })
+      })
     ])),
   down: async queryInterface => queryInterface.sequelize
     .transaction(transaction => Promise.all([
