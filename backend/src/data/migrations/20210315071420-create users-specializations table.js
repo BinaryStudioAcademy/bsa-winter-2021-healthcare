807ee40bafcb6a529c102bbb8b2f2a05341d29e9
ReznikYuriy <reznik333@gmail.com>
2021-03-15 12:09:52 +0200

    HC-21: *update migration create users-specializations table

--- backend/src/data/migrations/20210315071420-create users-specializations table.js
+++ backend/src/data/migrations/20210315071420-create users-specializations table.js
@@ -1,21 +1,33 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.sequelize.transaction(transaction => Promise.all([
+      queryInterface.createTable('users_specializations', {
+        userId: {
+          type: Sequelize.UUID,
+          references: {
+            model: 'users',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL'
+        },
+        specializationId: {
+          type: Sequelize.UUID,
+          references: {
+            model: 'specializations',
+            key: 'id'
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL'
+        },
+        createdAt: Sequelize.DATE,
+        updatedAt: Sequelize.DATE
+      }, { transaction })
+    ])),
+  down: async queryInterface => queryInterface.sequelize
+    .transaction(transaction => Promise.all([
+      queryInterface.dropTable('users_specializations', { transaction })
+    ]))
 };
