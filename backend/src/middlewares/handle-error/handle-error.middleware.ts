f033a80409873135b4c01f8afc62796b3338913c
Dnzln <qvanoman@gmail.com>
2021-03-15 13:50:00 +0200

    HC24: * pass string to logger

--- backend/src/middlewares/handle-error/handle-error.middleware.ts
+++ backend/src/middlewares/handle-error/handle-error.middleware.ts
@@ -4,9 +4,9 @@ import { HttpError } from '~/exceptions';
 import { HttpCode } from '~/common/enums';
 
 const handleError: ErrorRequestHandler = (err: HttpError, _req, res, _next) => {
-  const { status = HttpCode.INTERNAL_SERVER_ERROR, messages, stack } = err;
+  const { status = HttpCode.INTERNAL_SERVER_ERROR, message, messages, stack } = err;
 
-  logger.error(messages, stack);
+  logger.error(message, stack);
 
   return res.status(status).send({
     messages,
