e3b9024b9a04aa24beae18e00940617b9403347b
ReznikYuriy <reznik333@gmail.com>
2021-03-15 18:24:43 +0200

    HC-21:*+specialization_id to foreingKey enum

--- backend/src/common/enums/db/foreing-key.enum.ts
+++ backend/src/common/enums/db/foreing-key.enum.ts
@@ -3,7 +3,8 @@ enum ForeingKey {
   DOCTOR_ID = 'doctorId',
   CLINIC_ID = 'clinicId',
   DOCUMENT_ID = 'documentId',
-  PERMISSION_ID = 'permissionId'
+  PERMISSION_ID = 'permissionId',
+  SPECIALIZATION_ID = 'specializationId'
 }
 
 export { ForeingKey };
