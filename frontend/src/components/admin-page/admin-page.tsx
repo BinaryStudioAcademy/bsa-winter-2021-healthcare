92957e7ba0cad040bd357ada4acac9f2571066f0
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-15 23:18:06 +0200

    HC-7 * rename all own function

--- frontend/src/components/admin-page/admin-page.tsx
+++ frontend/src/components/admin-page/admin-page.tsx
@@ -2,50 +2,47 @@ import * as React from 'react';
 import { useDispatch } from 'react-redux';
 import { UsersActionCreator } from 'store/slices';
 import styles from './styles.module.scss';
-import AdminTable from './components/admin-table/admin-table';
-import CreateUser from './create-user';
-import EditUser from './edit-user';
+import {AdminTable, CreateUserPopup, EditUserPopup} from './components';
 import { IEditUserPayload, IRegisterPayload, IUser } from 'common/interfaces';
 import { DEFAULT_USER_INSTANCE } from 'components/admin-page/constants';
 import { Modal } from 'components/common';
 
 const AdminPage: React.FC = () => {
   const [user, setUser] = React.useState<IUser>(DEFAULT_USER_INSTANCE);
-  const [showPopUp, setShowPopUp] = React.useState<boolean>(false);
-  const userWithDate: IEditUserPayload = {...user,birthdate:new Date(user.birthdate)};
+  const [isShowPopUp, setIsShowPopUp] = React.useState<boolean>(false);
 
   const dispatch = useDispatch();
   const handleEditUser = (data: IEditUserPayload) => {
     dispatch(UsersActionCreator.editUser({...data,id:user.id}));
     hideForm();
   };
-  const addUser = (data: IRegisterPayload) => {
+  const handleAddUser = (data: IRegisterPayload) => {
     const newUser: IUser = { ...user, ...data };
     dispatch(UsersActionCreator.addUser(newUser));
     hideForm();
   };
-  const deleteUser = (id: string) => {
+  const handleDeleteUser = (id: string) => {
     dispatch(UsersActionCreator.deleteUser(id));
   };
 
   const showFormHandler = (user: IUser) => {
     user
-      ? (setUser({ ...DEFAULT_USER_INSTANCE, ...user }), setShowPopUp(true))
-      : setShowPopUp(true);
+      ? (setUser({ ...DEFAULT_USER_INSTANCE, ...user }), setIsShowPopUp(true))
+      : setIsShowPopUp(true);
   };
   const hideForm = () => {
     setUser(DEFAULT_USER_INSTANCE);
-    setShowPopUp(false);
+    setIsShowPopUp(false);
   };
   return (
     <div className={styles.container}>
       {/* <button onClick={()=>showFormHandler()}>asd</button> */}
-      <AdminTable onFormShow={showFormHandler} onUserDelete={deleteUser} />
-      <Modal isShow={showPopUp}>
+      <AdminTable onFormShow={showFormHandler} onUserDelete={handleDeleteUser} />
+      <Modal isShow={isShowPopUp}>
         {user.id ? (
-          <EditUser user={userWithDate} onEditUser={handleEditUser} onFormHide={hideForm} />
+          <EditUserPopup user={user} onEditUser={handleEditUser} onFormHide={hideForm} />
         ) : (
-          <CreateUser onCreateUser={addUser} onFormHide={hideForm} />
+          <CreateUserPopup onCreateUser={handleAddUser} onFormHide={hideForm} />
         )}
       </Modal>
     </div>
