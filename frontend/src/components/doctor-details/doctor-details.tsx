9d5c31d0615c427c2c36f138df143a7750138391
ReznikYuriy <reznik333@gmail.com>
2021-03-16 16:23:10 +0200

    HC-21: *update doctor-details component

--- frontend/src/components/doctor-details/doctor-details.tsx
+++ frontend/src/components/doctor-details/doctor-details.tsx
@@ -1,85 +1,60 @@
 import * as React from 'react';
 import phoneIcon from 'assets/images/icons/phone.svg';
 import checkIcon from 'assets/images/icons/check.svg';
-
+import { RootState } from 'common/types';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { DoctorsActionCreator } from 'store/slices';
 import styles from './styles.module.scss';
+import { DataStatus } from 'common/enums';
+import { useLocation } from 'react-router-dom';
 
-/*  type Props = {
-  doctor:IUserTypeDoctor
-}  */
-const doctor = {
-  imagePath: '/images/photo.jpg',
-  name: 'Vasilisa',
-  surname: 'Frolova',
-  department: 'Pediatrician',
-  phone: '+10666666666',
-  about: 'Капитан Испаньолы, мужественный и честный человек, занимающийся не только навигацией, но обустройством быта на корабле. Этот требовательный и сухой человек был нанят сквайром Трелони. Стреляет отвратительно, но зато прекрасно владеет холодным оружием.',
-  specializations: [{
-    id: '1',
-    text: 'Conditions Treated',
-  },
-  {
-    id: '2',
-    text: 'Chest Wall Deformities',
-  },
-  {
-    id: '3',
-    text: 'General Pediatric Surgery',
-  },
-  {
-    id: '4',
-    text: 'Neonatal Minimally Invasive Surgery',
-  },
-  {
-    id: '5',
-    text: 'Pediatric Adrenal Masses',
-  },
-  {
-    id: '6',
-    text: 'Pediatric Parathyroid Tumors',
-  },
-  {
-    id: '7',
-    text: 'Pediatric Thyroid Tumors',
-  },
-  {
-    id: '8',
-    text: 'Thoracic Pediatric Surgery',
-  }]
-};
 const DoctorDetails: React.FC = () => {
+
+  const { doctorDetails, dataStatus } = useSelector(({ doctors }: RootState) => ({
+    doctorDetails: doctors.doctorDetails,
+    dataStatus: doctors.dataStatus
+  }));
+  const dispatch = useDispatch();
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    dispatch(DoctorsActionCreator.getDoctorDetailsAsync(pathname))
+  }, [])
 
   return (
     <div className={styles.doctorsDetailsContainer}>
-
-      <div className={styles.personalDataContainer}>
-        <img className={styles.doctorImage} src={doctor.imagePath} alt={doctor.name} />
-        <div className={styles.personalDataBody}>
-          <span className={styles.department}>{doctor.department}</span>
-          <div className={styles.phone}>
-            <img src={phoneIcon} alt="phone-icon" />
-            <span className={styles.phoneNumber}>{doctor.phone}</span>
+      {dataStatus === DataStatus.PENDING && <div>...Loading</div>}
+      {dataStatus === DataStatus.SUCCESS &&
+        <>
+          <div className={styles.personalDataContainer}>
+            <img className={styles.doctorImage} src={doctorDetails.imagePath} alt={doctorDetails.name} />
+            <div className={styles.personalDataBody}>
+              <span className={styles.department}>{doctorDetails.doctor.department}</span>
+              <div className={styles.phone}>
+                <img src={phoneIcon} alt="phone-icon" />
+                <span className={styles.phoneNumber}>{doctorDetails.phone}</span>
+              </div>
+              <span className={styles.name}>{doctorDetails.name} {doctorDetails.surname}</span>
+            </div>
           </div>
-          <span className={styles.name}>{doctor.name} {doctor.surname}</span>
-        </div>
-      </div>
-      <div className={styles.aboutContainer}>
-        <span className={styles.aboutTitle}>About {doctor.name} {doctor.surname}</span>
-        <div className={styles.aboutBody}>{doctor.about}</div>
-      </div>
+          <div className={styles.aboutContainer}>
+            <span className={styles.aboutTitle}>About {doctorDetails.name} {doctorDetails.surname}</span>
+            <div className={styles.aboutBody}>{doctorDetails.doctor.about}</div>
+          </div>
 
-      <div className={styles.conditionsContainer}>
-        <div className={styles.conditionsTitle}>
-          <img src={checkIcon} alt="check-icon" />
-          <span className={styles.titleText}>Conditions Treated</span>
-        </div>
-        <ul className={styles.conditionsList}>
-          {doctor.specializations.map(spec =>
-            <li key={spec.id}>{spec.text}</li>
-          )}
-        </ul>
-      </div>
-
+          <div className={styles.conditionsContainer}>
+            <div className={styles.conditionsTitle}>
+              <img src={checkIcon} alt="check-icon" />
+              <span className={styles.titleText}>Conditions Treated</span>
+            </div>
+            <ul className={styles.conditionsList}>
+              {doctorDetails.specializations.map(spec =>
+                <li key={spec.id}>{spec.text}</li>
+              )}
+            </ul>
+          </div>
+        </>}
     </div>
   );
 };
