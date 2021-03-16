0e9fbe23ac71afa5ae7fe35677358c8fbd01f008
ReznikYuriy <reznik333@gmail.com>
2021-03-16 16:21:12 +0200

    HC-21: add doctorDetails fetching to doctor.slice

--- frontend/src/store/slices/doctors/doctors.slice.ts
+++ frontend/src/store/slices/doctors/doctors.slice.ts
@@ -2,17 +2,43 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { ReducerName, DataStatus } from 'common/enums';
 import { AppThunk } from 'common/types';
 import { userApi, notificationService } from 'services';
-import { IUserTypeDoctor } from 'common/interfaces'
+import { IDoctorDetails, IUserTypeDoctor } from 'common/interfaces';
 import { HttpError } from 'exceptions';
+import {
+  DoctorDetailsKey,
+  DoctorKey,
+  UserKey,
+  UserSex,
+  UserType,
+} from 'healthcare-shared/common/enums';
 
 type DoctorsState = {
-  doctors: IUserTypeDoctor[]
-  dataStatus: DataStatus
+  doctors: IUserTypeDoctor[];
+  doctorDetails: IDoctorDetails;
+  dataStatus: DataStatus;
 };
 
 const initialState: DoctorsState = {
   doctors: [],
-  dataStatus: DataStatus.PENDING
+  doctorDetails: {
+    [UserKey.NAME]: '',
+    [UserKey.SURNAME]: '',
+    [UserKey.IMAGE_PATH]: '',
+    [UserKey.PHONE]: '',
+    [UserKey.BIRTHDATE]: '',
+    [UserKey.SEX]: UserSex.MALE,
+    [UserKey.TYPE]: UserType.DOCTOR,
+    [UserKey.EMAIL]: '',
+    [UserKey.PASSWORD]: '',
+    [UserKey.CREATED_AT]: '',
+    [UserKey.UPDATED_AT]: '',
+    doctor: {
+      [DoctorKey.DEPARTMENT]: '',
+      [DoctorKey.ABOUT]: '',
+    },
+    [DoctorDetailsKey.SPECIALIZATIONS]: [],
+  },
+  dataStatus: DataStatus.PENDING,
 };
 
 const { reducer, actions } = createSlice({
@@ -21,6 +47,10 @@ const { reducer, actions } = createSlice({
   reducers: {
     setDoctors: (state, action: PayloadAction<IUserTypeDoctor[]>) => {
       state.doctors = action.payload;
+      state.dataStatus = DataStatus.SUCCESS;
+    },
+    setDoctorDetail: (state, action: PayloadAction<IDoctorDetails>) => {
+      state.doctorDetails = action.payload;
       state.dataStatus = DataStatus.SUCCESS;
     },
   },
@@ -38,9 +68,22 @@ const getDoctorsAsync = (): AppThunk => async (dispatch) => {
   }
 };
 
+const getDoctorDetailsAsync = (id: string): AppThunk => async (dispatch) => {
+  try {
+    const doctorDetails = await userApi.getDoctorDetails(id);
+    dispatch(actions.setDoctorDetail(doctorDetails));
+  } catch (error) {
+    if (error instanceof HttpError) {
+      notificationService.error(`Error ${error.status}`, error.messages);
+    }
+    throw error;
+  }
+};
+
 const DoctorsActionCreator = {
   ...actions,
-  getDoctorsAsync
+  getDoctorsAsync,
+  getDoctorDetailsAsync,
 };
 
 export { DoctorsActionCreator, reducer };
