57334461e7a3dc5c7959f464393d5514c28b91c2
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-03-16 16:08:24 +0200

    HC-15: * set icon as background image

--- frontend/src/components/common/details/details.tsx
+++ frontend/src/components/common/details/details.tsx
@@ -1,21 +1,24 @@
 import * as React from 'react';
-import clsx from 'clsx';
 import styles from './styles.module.scss';
 
 interface Props {
   icon: string;
   title: string;
-  children: React.ReactNode;
 }
 
 const Details: React.FC<Props> = ({ icon, title, children }) => {
   return (
-    <details className={clsx(styles.commonFilter)}>
-      <summary className={clsx(styles.filterInfo)}>
-        <img src={icon} loading="lazy" alt="Filter icon."/>
-        <div className={clsx(styles.filterName)}>{title}</div>
+    <details className={styles.commonFilter}>
+      <summary className={styles.filterInfo}>
+        <div
+          className={styles.icon}
+          style={{
+            backgroundImage: `url(${icon})`
+          }}
+        ></div>
+        <div className={styles.filterName}>{title}</div>
       </summary>
-      <div className={clsx(styles.content)}>
+      <div className={styles.content}>
         {children}
       </div>
     </details>
