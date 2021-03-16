d71da99dd686a49bbc34c420bbd1535236e031b2
ReznikYuriy <reznik333@gmail.com>
2021-03-16 16:19:11 +0200

    HC-21: +getDoctorDetails metod to userService

--- frontend/src/services/user-api/user-api.service.ts
+++ frontend/src/services/user-api/user-api.service.ts
@@ -1,6 +1,6 @@
 import { Http } from '../http/http.service';
 import { HttpMethod, UsersApiPath, UserType, ApiPath } from 'common/enums';
-import { IUserTypeDoctor } from 'common/interfaces';
+import { IDoctorDetails, IUserTypeDoctor } from 'common/interfaces';
 
 type Constructor = {
   http: Http;
@@ -17,6 +17,12 @@ class UserApi {
 
   public getDoctors(): Promise<IUserTypeDoctor[]> {
     return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}${UsersApiPath.TYPE}/${UserType.DOCTOR}`, {
+      method: HttpMethod.GET
+    });
+  }
+
+  public getDoctorDetails(id:string): Promise<IDoctorDetails> {
+    return this.#http.load(`${this.#apiPrefix}${ApiPath.USERS}/${id}`, {
       method: HttpMethod.GET
     });
   }
