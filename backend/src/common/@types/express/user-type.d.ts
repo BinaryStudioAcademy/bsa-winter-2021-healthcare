b5dee5cbabf9c0b08f78223a76a9b83520bbf757
Yarikks <jasper18poil@gmail.com>
2021-03-17 22:37:53 +0200

    HC-17: * allow single extends for interface

--- backend/src/common/@types/express/user-type.d.ts
+++ backend/src/common/@types/express/user-type.d.ts
@@ -2,7 +2,6 @@ import { IUser } from '~/common/interfaces';
 
 declare global {
   namespace Express {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     interface User extends IUser {}
     interface Request {
       user?: IUser;
