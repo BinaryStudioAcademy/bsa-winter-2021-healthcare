5dc82db4f853a0db5504814dcad6b63aca5f5dc8
Yarikks <jasper18poil@gmail.com>
2021-03-17 16:58:00 +0200

    HC-17: + add authorization middleware and maku use of it

---
+++ backend/src/middlewares/auth/authorization.middleware.ts
@@ -0,0 +1,15 @@
+import { Response, Request, NextFunction } from 'express';
+import { AuthApiPath } from '~/common/enums';
+import { jwtMiddleWare } from './jwt.middleware';
+
+const authorizationMiddleware = (routesWhiteList: AuthApiPath[] = []) => (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  routesWhiteList.some((route) => route === req.path)
+    ? next()
+    : jwtMiddleWare(req, res, next);
+};
+
+export { authorizationMiddleware };
--- backend/src/middlewares/auth/index.ts
+++ backend/src/middlewares/auth/index.ts
@@ -1,3 +1,4 @@
 export * from './authentication.middleware';
 export * from './registration.middleware';
 export * from './jwt.middleware';
+export * from './authorization.middleware';
