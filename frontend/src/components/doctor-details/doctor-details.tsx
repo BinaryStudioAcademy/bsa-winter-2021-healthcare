078975f32ffebf19c7e88167718b75df30992ecb
ReznikYuriy <reznik333@gmail.com>
2021-03-17 14:38:57 +0200

    HC-21: *add check doctorDetails!==null

--- frontend/src/components/doctor-details/doctor-details.tsx
+++ frontend/src/components/doctor-details/doctor-details.tsx
@@ -23,27 +23,27 @@ const DoctorDetails: React.FC = () => {
     dispatch(DoctorsActionCreator.getDoctorDetailsAsync(id))
   }, [])
 
-  if (dataStatus === DataStatus.PENDING) {
+  if (dataStatus === DataStatus.PENDING || !doctorDetails) {
     return <div>...Loading</div>;
   }
 
   return (
     <div className={styles.doctorsDetailsContainer}>
       <div className={styles.personalDataContainer}>
-        <img className={styles.doctorImage} src={doctorDetails?.imagePath} width="58" height="60" loading="lazy"
-          alt={doctorDetails?.name} />
+        <img className={styles.doctorImage} src={doctorDetails.imagePath} width="58" height="60" loading="lazy"
+          alt={doctorDetails.name} />
         <div className={styles.personalDataBody}>
-          <span className={styles.department}>{doctorDetails?.doctor.department}</span>
+          <span className={styles.department}>{doctorDetails.doctor.department}</span>
           <div className={styles.phone}>
             <img src={phoneIcon} width="22" height="22" loading="lazy" alt="phone-icon" />
-            <span className={styles.phoneNumber}>{doctorDetails?.phone}</span>
+            <span className={styles.phoneNumber}>{doctorDetails.phone}</span>
           </div>
-          <span className={styles.name}>{doctorDetails?.name} {doctorDetails?.surname}</span>
+          <span className={styles.name}>{doctorDetails.name} {doctorDetails.surname}</span>
         </div>
       </div>
       <div className={styles.aboutContainer}>
-        <span className={styles.aboutTitle}>About {doctorDetails?.name} {doctorDetails?.surname}</span>
-        <div className={styles.aboutBody}>{doctorDetails?.doctor.about}</div>
+        <span className={styles.aboutTitle}>About {doctorDetails.name} {doctorDetails.surname}</span>
+        <div className={styles.aboutBody}>{doctorDetails.doctor.about}</div>
       </div>
 
       <div className={styles.conditionsContainer}>
@@ -52,7 +52,7 @@ const DoctorDetails: React.FC = () => {
           <span className={styles.titleText}>Conditions Treated</span>
         </div>
         <ul className={styles.conditionsList}>
-          {doctorDetails?.specializations.map(spec =>
+          {doctorDetails.specializations.map(spec =>
             <li key={spec.id}>{spec.text}</li>,
           )}
         </ul>
