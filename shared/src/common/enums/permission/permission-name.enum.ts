ea62f3328fa8f82bfc47b8b49383312cefb8e19e
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-17 17:55:23 +0200

    HC-42 + add permission edit-permission

--- shared/src/common/enums/permission/permission-name.enum.ts
+++ shared/src/common/enums/permission/permission-name.enum.ts
@@ -2,6 +2,7 @@ enum PermissionName {
   CREATE_USER = 'create-user',
   EDIT_USER = 'edit-user',
   CREATE_CLINIC = 'create-clinic',
+  EDIT_PERMISSIONS = 'edit-permissions',
 }
 
 export { PermissionName };
