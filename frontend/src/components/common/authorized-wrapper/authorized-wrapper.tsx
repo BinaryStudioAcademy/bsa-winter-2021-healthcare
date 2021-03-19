4513cd5cfb7e782ff0e36f776d14fcddbefa0b42
Dnzln <qvanoman@gmail.com>
2021-03-19 20:31:47 +0200

    HC37: * add covid btn component to authorized wrapper

--- frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
+++ frontend/src/components/common/authorized-wrapper/authorized-wrapper.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { RootState } from 'common/types';
 import { useSelector } from 'react-redux';
+import { RootState } from 'common/types';
+import { PermissionName } from 'common/enums';
 import { Header } from 'components/common';
+import CovidButton from 'components/covid-button/covid-button';
 import { IUserWithPermissions } from 'common/interfaces';
+import { checkHasPermission } from '../authorized-route/helpers';
 
 const AuthorizedWrapper: React.FC = ({ children }) => {
   const { user } = useSelector(({ auth }: RootState) => ({
     user: auth.user,
   }));
 
+  const hasMapPermission = checkHasPermission([PermissionName.MAP_MANIPULATION], user?.permissions ?? []);
+
   return (
     <div>
       <Header user={user as IUserWithPermissions} />
       {children}
+      {hasMapPermission && <CovidButton/>}
     </div>
   );
 };
