8ca029a75b3fcedd921f414500ff64137e374547
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-20 13:35:46 +0200

    HC-42 + add edit-permissions permission for admin

--- backend/src/data/migrations/20210319123658-add-seed-data-edit-permission.js
+++ backend/src/data/migrations/20210319123658-add-seed-data-edit-permission.js
@@ -6,22 +6,47 @@ module.exports = {
       Promise.all([
         queryInterface.bulkInsert(
           'permissions',
-          [
-            {
-              name: 'edit-permissions',
-              createdAt: new Date(),
-              updatedAt: new Date(),
-            },
-          ],
-          { transaction },
-        ),
+          [{
+            name: 'edit-permissions',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          }, ], {
+            transaction,
+            returning: true
+          },
+        ).then((permission) => {
+          queryInterface.sequelize
+            .query('SELECT * FROM users')
+            .then((users) => {
+              const adminData = users[0].filter((user) => user.email === 'admin@admin.com');
+              return queryInterface.bulkInsert(
+                'users_permissions', 
+                [{
+                  userId: adminData[0].id,
+                  permissionId: permission[0].id,
+                  createdAt: new Date(),
+                  updatedAt: new Date(),
+                }]
+              )
+            }).catch((e) => {
+              console.log(e)
+            });
+        })
       ]),
     ),
-  down: async (queryInterface, Sequelize) =>{
+  down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
     queryInterface.sequelize.transaction((transaction) =>
       Promise.all([
-        queryInterface.bulkDelete('permissions', {[Op.or]: [{name: 'edit-permissions'}, {name: 'edit-permissions'}]}, { transaction }),
+        queryInterface.bulkDelete('permissions', {
+          [Op.or]: [{
+            name: 'edit-permissions'
+          }, {
+            name: 'edit-permissions'
+          }]
+        }, {
+          transaction
+        }),
       ]),
     )
   }
