38047061fa8b6c1da2abb75e42719d42660e9c9c
Dnzln <qvanoman@gmail.com>
2021-03-21 00:14:53 +0200

    HC37: * remove checking location in covid btn component

--- frontend/src/components/covid-button/covid-button.tsx
+++ frontend/src/components/covid-button/covid-button.tsx
@@ -1,26 +1,18 @@
 import * as React from 'react';
-import {
-  useLocation,
-} from 'react-router-dom';
 import { AppRoute, ButtonColor, ButtonStyleType } from 'common/enums';
 import { Button } from 'components/common';
 import styles from './styles.module.scss';
 
-const CovidButton: React.FC = () => {
-  const location = useLocation();
-  const isMapPage = location.pathname == AppRoute.MAP;
-
-  return isMapPage ? null : (
-    <div className={styles.covidButtonBlock}>
-      <Button
-        styleType={ButtonStyleType.EXTRA_LARGE_ROUND}
-        color={ButtonColor.RED}
-        label={'COVID-19'}
-        hasHiddenLabel={false}
-        href={AppRoute.MAP}
-      />
-    </div>
-  );
-};
+const CovidButton: React.FC = () => (
+  <div className={styles.covidButtonBlock}>
+    <Button
+      styleType={ButtonStyleType.EXTRA_LARGE_ROUND}
+      color={ButtonColor.RED}
+      label={'COVID-19'}
+      hasHiddenLabel={false}
+      href={AppRoute.MAP}
+    />
+  </div>
+);
 
 export default CovidButton;
