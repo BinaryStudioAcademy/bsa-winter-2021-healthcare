fdb156e6aaa3a052b0d7382237f4e0b66f57caf2
Dnzln <qvanoman@gmail.com>
2021-03-21 00:15:54 +0200

    HC37: * refactor select map area component

--- frontend/src/components/map/components/select-map-area/select-map-area.tsx
+++ frontend/src/components/map/components/select-map-area/select-map-area.tsx
@@ -5,8 +5,8 @@ import { useMap } from 'react-leaflet';
 import L, { LatLng, LeafletEvent } from 'leaflet';
 import 'leaflet-lasso';
 import { FINISHED_EVENT, ENABLED_EVENT, LassoHandlerFinishedEvent } from 'leaflet-lasso';
-import { MapCoordsDefault } from 'common/enums';
 import { MapActionCreator } from 'store/slices';
+import { MapCoordsDefault } from 'components/map/enums';
 
 const SelectMapArea: React.FC = () => {
   const dispatch = useDispatch();
@@ -17,10 +17,6 @@ const SelectMapArea: React.FC = () => {
   let maxLat = MapCoordsDefault.MAX_LAT;
   let maxLng = MapCoordsDefault.MAX_LNG;
   let selectedAreaPolygon = L.polygon([]);
-
-  const handleLassoAddition = (): void => {
-    control.addTo(map);
-  };
 
   const handleLassoActivacion = (): void => {
     minLat = MapCoordsDefault.MIN_LAT;
@@ -33,10 +29,21 @@ const SelectMapArea: React.FC = () => {
   const handleAreaSelection = (evt: LeafletEvent): void => {
     const lassoEvent = evt as LassoHandlerFinishedEvent;
     lassoEvent.latLngs.forEach((coords: LatLng) => {
-      if (coords.lat < minLat) minLat = coords.lat;
-      if (coords.lat > maxLat) maxLat = coords.lat;
-      if (coords.lng < minLng) minLng = coords.lng;
-      if (coords.lng > maxLng) maxLng = coords.lng;
+      if (coords.lat < minLat) {
+        minLat = coords.lat;
+      }
+
+      if (coords.lat > maxLat) {
+        maxLat = coords.lat;
+      }
+
+      if (coords.lng < minLng) {
+        minLng = coords.lng;
+      }
+
+      if (coords.lng > maxLng) {
+        maxLng = coords.lng;
+      }
     });
 
     selectedAreaPolygon = L.polygon([
@@ -56,7 +63,10 @@ const SelectMapArea: React.FC = () => {
     }));
   };
 
-  useEffect(handleLassoAddition);
+  useEffect((): void => {
+    control.addTo(map);
+  }, []);
+
   map.on(ENABLED_EVENT, handleLassoActivacion);
   map.on(FINISHED_EVENT, handleAreaSelection);
 
