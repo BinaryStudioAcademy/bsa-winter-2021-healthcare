25f56ba3f2227bd7686ec18fc019b45e194fc80f
Yarikks <jasper18poil@gmail.com>
2021-03-20 18:26:56 +0200

    HC-43: + add diagnosis api

--- backend/src/api/api.ts
+++ backend/src/api/api.ts
@@ -4,8 +4,15 @@ import { initUserApi } from './user/user.api';
 import { initAuthApi } from './auth/auth.api';
 import { initClinicApi } from './clinic/clinic.api';
 import { initGeolocationApi } from './geolocation/geolocation.api';
+import { initDiagnosisApi } from './diagnosis/diagnosis.api';
 
-const apis = [initUserApi, initAuthApi, initClinicApi, initGeolocationApi];
+const apis = [
+  initUserApi,
+  initAuthApi,
+  initClinicApi,
+  initGeolocationApi,
+  initDiagnosisApi,
+];
 
 const initApi = (app: Router): Router => {
   const apiRouter = Router();
---
+++ backend/src/api/diagnosis/diagnosis.api.ts
@@ -0,0 +1,40 @@
+import { Router } from 'express';
+import { ApiPath, DiagnosesApiPath, HttpCode } from '~/common/enums';
+import { diagnosis as diagnosisService } from '~/services/services';
+
+const initDiagnosisApi = (apiRouter: Router): Router => {
+  const diagnosisRouter = Router();
+
+  apiRouter.use(ApiPath.DIAGNOSES, diagnosisRouter);
+
+  diagnosisRouter.get(DiagnosesApiPath.USERS_$ID, async (req, res, next) => {
+    try {
+      const diagnoses = await diagnosisService.getAllByUserId(req.params.id);
+      res.status(HttpCode.OK).json(diagnoses);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  diagnosisRouter.post(DiagnosesApiPath.ROOT, async (req, res, next) => {
+    try {
+      const diagnosis = await diagnosisService.create(req.body);
+      res.status(HttpCode.CREATED).json(diagnosis);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  diagnosisRouter.put(DiagnosesApiPath.$ID, async (req, res, next) => {
+    try {
+      const diagnosis = await diagnosisService.update(req.params.id, req.body);
+      res.status(HttpCode.OK).json(diagnosis);
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  return diagnosisRouter;
+};
+
+export { initDiagnosisApi };
