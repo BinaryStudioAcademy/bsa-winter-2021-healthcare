591015ca41761719b52482670425dab8e4a06a44
ReznikYuriy <reznik333@gmail.com>
2021-03-21 20:04:50 +0200

    HC-52: *correct IDoctor instance

--- shared/src/common/interfaces/doctor/doctor.instance.ts
+++ shared/src/common/interfaces/doctor/doctor.instance.ts
@@ -1,8 +1,8 @@
 import { DoctorKey } from '~/common/enums';
 
 interface IDoctor {
-  [DoctorKey.ID]:string
-  [DoctorKey.ABOUT]: string,
+  [DoctorKey.ID]: string;
+  [DoctorKey.ABOUT]: string;
   [DoctorKey.CREATED_AT]: string;
   [DoctorKey.UPDATED_AT]: string;
 }
