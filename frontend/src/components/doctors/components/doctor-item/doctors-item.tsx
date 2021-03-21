ac386d3f6d4e35bbce34f07a8acc18ec3dead5e4
ReznikYuriy <reznik333@gmail.com>
2021-03-21 20:09:37 +0200

    HC-52: -remove doctor.department from doctors-item

--- frontend/src/components/doctors/components/doctor-item/doctors-item.tsx
+++ frontend/src/components/doctors/components/doctor-item/doctors-item.tsx
@@ -11,7 +11,7 @@ type Props = {
 const DoctorItem: React.FC<Props> = ({ user }) => {
   return (
     <Card
-      subtitle={user.doctor.department}
+      subtitle=""
       title={`${user.name} ${user.surname}`}
       label={user.doctor.clinic.clinicType}
       btnLabel="Make an appointment"
