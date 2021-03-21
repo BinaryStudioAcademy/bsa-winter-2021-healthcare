3922f6723596ec694ac6c75835ea0aab2deea7fc
Daryna Matiiuk <dasha.matiyuk@gmail.com>
2021-03-21 19:13:27 +0200

    HC-40: + add notifications api path

--- shared/src/common/enums/api/api-path.enum.ts
+++ shared/src/common/enums/api/api-path.enum.ts
@@ -3,6 +3,7 @@ enum ApiPath {
   AUTH = '/auth',
   CLINICS = '/clinics',
   GEOLOCATIONS = '/geolocations',
+  NOTIFICATIONS = '/notifications',
 }
 
 export { ApiPath };
---
+++ shared/src/common/enums/api/notifications-api-path.enum.ts
@@ -0,0 +1,5 @@
+enum NotificationsApiPath {
+  ROOT = '/',
+}
+
+export { NotificationsApiPath };
