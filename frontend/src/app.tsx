f4f226e9a01a4b676903385370843e177bd54ca6
Yarikks <jasper18poil@gmail.com>
2021-03-22 11:21:22 +0200

    HC-43: * change links in header

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -13,11 +13,21 @@ import { AuthorizedRoute } from 'components/common';
 const App: React.FC = () => (
   <Switch>
     <Route path={[AppRoute.SIGN_IN, AppRoute.SIGN_UP]} component={Sign} />
-    <AuthorizedRoute path={AppRoute.USER_PROFILE_$ID} component={Profile} />
+    <AuthorizedRoute
+      path={AppRoute.USER_PROFILE_$ID}
+      exact
+      component={Profile}
+    />
     <AuthorizedRoute path={AppRoute.CLINICS} component={Clinics} />
     <AuthorizedRoute path={AppRoute.DOCTORS} component={Doctors} />
-    <AuthorizedRoute path={AppRoute.DOCTOR_DETAILS_$ID} component={DoctorDetails} />
-    <AuthorizedRoute exact path={AppRoute.USERS} component={Users}
+    <AuthorizedRoute
+      path={AppRoute.DOCTOR_DETAILS_$ID}
+      component={DoctorDetails}
+    />
+    <AuthorizedRoute
+      exact
+      path={AppRoute.USERS}
+      component={Users}
       permissions={[PermissionName.CREATE_USER, PermissionName.EDIT_USER]}
     />
     <Route path="*" exact component={NotFound} />
