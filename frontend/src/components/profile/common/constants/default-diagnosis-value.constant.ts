4d329b6003b4493babfc7cd77a2c634a8d4a2a41
Yarikks <jasper18poil@gmail.com>
2021-03-23 15:22:09 +0200

    HC-43: * rewrite diagnoses component, change default values

--- frontend/src/components/profile/common/constants/default-diagnosis-value.constant.ts
+++ frontend/src/components/profile/common/constants/default-diagnosis-value.constant.ts
@@ -1,7 +1,13 @@
 import { DiagnosisKey } from 'common/enums';
+import { IDiagnosis } from 'common/interfaces';
 
-const DEFAULT_DIAGNOSIS_VALUE = {
+const DEFAULT_DIAGNOSIS_VALUE: IDiagnosis = {
+  [DiagnosisKey.ID]: '',
   [DiagnosisKey.DIAGNOSIS]: '',
+  [DiagnosisKey.DESCRIPTION]: '',
+  [DiagnosisKey.USER_ID]: '',
+  [DiagnosisKey.CREATED_AT]: '',
+  [DiagnosisKey.UPDATED_AT]: '',
 };
 
 export { DEFAULT_DIAGNOSIS_VALUE };
