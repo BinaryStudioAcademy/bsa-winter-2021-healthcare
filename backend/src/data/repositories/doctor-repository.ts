e2affba7400ec4e7cf05a93e0327350ba96cb30e
Denysiuk Yaroslav <denusyk3@gmail.com>
2021-03-25 14:43:45 +0200

    HC-55 * add addDoctorToClinic service

--- backend/src/data/repositories/doctor-repository.ts
+++ backend/src/data/repositories/doctor-repository.ts
@@ -5,6 +5,13 @@ class DoctorRepository {
   public createDoctor(doctor: IDoctorRegisterPayload): Promise<IDoctor> {
     return DoctorModel.create(doctor);
   }
+  public async addDoctorToClinic(doctorId:string,clinicId:string): Promise<boolean>{
+      const [ , [doctor]] = await DoctorModel.update({clinicId}, {
+        where: { userId:doctorId },
+        returning: true,
+      });
+      return Boolean(doctor);
+  }
 }
 
 export { DoctorRepository };
--- backend/src/services/doctor-service/doctor-service.service.ts
+++ backend/src/services/doctor-service/doctor-service.service.ts
@@ -5,6 +5,10 @@ class Doctor {
   public createDoctor(doctorPayload: IDoctorRegisterPayload): Promise<IDoctor> {
     return doctorRepository.createDoctor(doctorPayload);
   }
+
+  public addDoctorToClinic(doctorId:string,clinicId:string): Promise<boolean>{
+    return doctorRepository.addDoctorToClinic(doctorId,clinicId);
+  }
 }
 
 export { Doctor };
