872dee4e19ad55be41e0192b751017eb28cfc849
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-25 18:30:45 +0200

    HC-65: + current-user path

--- shared/src/common/enums/api/users-api-path.enum.ts
+++ shared/src/common/enums/api/users-api-path.enum.ts
@@ -5,6 +5,7 @@ enum UsersApiPath {
   TYPE_$TYPE = '/type/:type',
   DOCTOR_DETAILS = '/doctor-details',
   DOCTOR_DETAILS_$ID = '/doctor-details/:id',
+  CURRENT_USER = '/current-user',
 }
 
 export { UsersApiPath };
