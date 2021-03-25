09e26d5f18d8e0e9d3e333ab7c273e9718558743
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-25 18:32:53 +0200

    HC-65: + refetch user after reload page

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -12,38 +12,48 @@ import Notifications from 'components/notifications/notifications';
 import Map from 'components/map/map';
 import Permissions from 'components/permissions/permissions-page';
 import { AuthorizedRoute } from 'components/common';
+import { AuthActionCreator } from 'store/slices';
+import { useDispatch } from 'react-redux';
 
-const App: React.FC = () => (
-  <Switch>
-    <Redirect from={AppRoute.ROOT} exact to={AppRoute.DOCTORS} />
-    <Route path={[AppRoute.SIGN_IN, AppRoute.SIGN_UP]} component={Sign} />
-    <AuthorizedRoute path={AppRoute.NOTIFICATIONS} component={Notifications} />
-    <AuthorizedRoute
-      path={AppRoute.USER_PROFILE_$ID}
-      exact
-      component={Profile}
-    />
-    <AuthorizedRoute path={AppRoute.CLINICS} component={Clinics} />
-    <AuthorizedRoute path={AppRoute.DOCTORS} component={Doctors} />
-    <AuthorizedRoute
-      path={AppRoute.DOCTOR_DETAILS_$ID}
-      component={DoctorDetails}
-    />
-    <AuthorizedRoute
-      exact
-      path={AppRoute.USERS}
-      component={Users}
-      permissions={[PermissionName.CREATE_USER, PermissionName.EDIT_USER]}
-    />
-    <AuthorizedRoute
-      exact
-      path={AppRoute.MAP}
-      component={Map}
-      permissions={[PermissionName.MAP_MANIPULATION]}
-    />
-    <AuthorizedRoute path={AppRoute.PERMISSIONS} component={Permissions} permissions={[PermissionName.EDIT_PERMISSIONS]} />
-    <Route path="*" exact component={NotFound} />
-  </Switch>
-);
+const App: React.FC = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(AuthActionCreator.getCurrentUser());
+  }, []);
+
+  return (
+    <Switch>
+      <Redirect from={AppRoute.ROOT} exact to={AppRoute.DOCTORS} />
+      <Route path={[AppRoute.SIGN_IN, AppRoute.SIGN_UP]} component={Sign} />
+      <AuthorizedRoute path={AppRoute.NOTIFICATIONS} component={Notifications} />
+      <AuthorizedRoute
+        path={AppRoute.USER_PROFILE_$ID}
+        exact
+        component={Profile}
+      />
+      <AuthorizedRoute path={AppRoute.CLINICS} component={Clinics} />
+      <AuthorizedRoute path={AppRoute.DOCTORS} component={Doctors} />
+      <AuthorizedRoute
+        path={AppRoute.DOCTOR_DETAILS_$ID}
+        component={DoctorDetails}
+      />
+      <AuthorizedRoute
+        exact
+        path={AppRoute.USERS}
+        component={Users}
+        permissions={[PermissionName.CREATE_USER, PermissionName.EDIT_USER]}
+      />
+      <AuthorizedRoute
+        exact
+        path={AppRoute.MAP}
+        component={Map}
+        permissions={[PermissionName.MAP_MANIPULATION]}
+      />
+      <AuthorizedRoute path={AppRoute.PERMISSIONS} component={Permissions} permissions={[PermissionName.EDIT_PERMISSIONS]} />
+      <Route path="*" exact component={NotFound} />
+    </Switch>
+  );
+};
 
 export default App;
