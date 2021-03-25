59938d197e2e8154dda8f1c82191b390d8793de0
Dnzln <qvanoman@gmail.com>
2021-03-25 04:30:07 +0200

    HC59: * add id fields to abstrations of doctor

--- shared/src/common/interfaces/doctor/doctor.instance.ts
+++ shared/src/common/interfaces/doctor/doctor.instance.ts
@@ -5,6 +5,9 @@ interface IDoctor {
   [DoctorKey.ABOUT]: string;
   [DoctorKey.CREATED_AT]: string;
   [DoctorKey.UPDATED_AT]: string;
+  [DoctorKey.PROFESSION_ID]?: string;
+  [DoctorKey.CLINIC_ID]?: string;
+  [DoctorKey.DOCUMENT_ID]?: string;
 }
 
 export type { IDoctor };
