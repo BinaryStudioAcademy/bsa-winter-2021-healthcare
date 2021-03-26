2be8bf81ec81cc429cb2f6696736a9b0452ca70f
Yurii Diakiv <yuradyakiv@gmail.com>
2021-03-26 19:24:04 +0200

    HC-86: + return type

--- frontend/src/helpers/default-avatar/default-avatar.ts
+++ frontend/src/helpers/default-avatar/default-avatar.ts
@@ -5,7 +5,7 @@ import defaultDoctorWoman from 'assets/images/default-doctor-woman.jpg';
 import defaultPatientMan from 'assets/images/default-patient-man.jpg';
 import defaultPatientWoman from 'assets/images/default-patient-woman.jpg';
 
-const getDefaultAvatar = (user: IUser) => {
+const getDefaultAvatar = (user: IUser): string => {
   return (
     (user.sex === UserSex.MALE ? (
       user.type === UserType.DOCTOR ? defaultDoctorMan : defaultPatientMan
